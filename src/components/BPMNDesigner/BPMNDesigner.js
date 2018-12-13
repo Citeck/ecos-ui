033d7a699193577a600e4f4db6145eee7e5c0cb9
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2018-12-13 15:40:49 +0300

    ECOSCOM-1876 - Create New BPMN designer section
    ViewSwitcher component; custom tooltip styles

--- src/components/BPMNDesigner/BPMNDesigner.js
+++ src/components/BPMNDesigner/BPMNDesigner.js
@@ -9,43 +9,41 @@ import './BPMNDesigner.scss';
 
 function BPMNDesigner() {
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col xs="8">
-            <div className={styles.header}>
-              <p className={styles.counter}>
-                Всего{' '}
-                <Badge color="primary" pill>
-                  0
-                </Badge>
-              </p>
-              <h2 className={styles.h2}>Модели бизнес процессов</h2>
-            </div>
-          </Col>
-        </Row>
-        <Row noGutters>
-          <Col xs="8">
-            <div className={styles.whiteBlock}>
-              <Button color="primary" size="lg" className={styles.headerBtn}>
-                Создать модель
-              </Button>
-              <Button color="secondary" size="lg" className={styles.headerBtn}>
-                Импортировать
-              </Button>
-            </div>
-            <ControlPanel />
-            <CategoriesList />
-            <div className={styles.addCategoryBlock}>
-              <a href="/share/page/bpmn-designer">Добавить категорию</a>
-            </div>
-          </Col>
-          <Col xs="4">
-            <RightMenu />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container>
+      <Row>
+        <Col xs="8">
+          <div className={styles.header}>
+            <p className={styles.counter}>
+              Всего{' '}
+              <Badge color="primary" pill>
+                0
+              </Badge>
+            </p>
+            <h2 className={styles.h2}>Модели бизнес процессов</h2>
+          </div>
+        </Col>
+      </Row>
+      <Row noGutters>
+        <Col xs="8">
+          <div className={styles.whiteBlock}>
+            <Button color="primary" size="lg" className={styles.headerBtn}>
+              Создать модель
+            </Button>
+            <Button color="secondary" size="lg" className={styles.headerBtn}>
+              Импортировать
+            </Button>
+          </div>
+          <ControlPanel />
+          <CategoriesList />
+          <div className={styles.addCategoryBlock}>
+            <a href="/share/page/bpmn-designer">Добавить категорию</a>
+          </div>
+        </Col>
+        <Col xs="4">
+          <RightMenu />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/components/BPMNDesigner/ControlPanel/ViewSwitcher/ViewSwitcher.js
+++ src/components/BPMNDesigner/ControlPanel/ViewSwitcher/ViewSwitcher.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { UncontrolledTooltip } from 'reactstrap';
 import cn from 'classnames';
 import styles from './ViewSwitcher.module.scss';
 
 const ViewSwitcher = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={cn('icon-tiles', styles.item, { [styles.itemActive]: true })} />
-      <div className={cn('icon-list', styles.item)} />
+      <div id="bpmn-view-switcher-cards" className={cn('icon-tiles', styles.item, { [styles.itemActive]: true })} />
+      <UncontrolledTooltip
+        target="bpmn-view-switcher-cards"
+        delay={0}
+        placement="top"
+        innerClassName="tooltip-inner-custom"
+        arrowClassName="arrow-custom"
+      >
+        Плитка
+      </UncontrolledTooltip>
+
+      <div id="bpmn-view-switcher-list" className={cn('icon-list', styles.item)} />
+      <UncontrolledTooltip
+        target="bpmn-view-switcher-list"
+        delay={0}
+        placement="top"
+        innerClassName="tooltip-inner-custom"
+        arrowClassName="arrow-custom"
+      >
+        Список
+      </UncontrolledTooltip>
     </div>
   );
 };
