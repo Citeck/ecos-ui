7c0d78d4f2f770f9e5d80d18e5c1c18a7c4b71fc
Dmitriy Shepelev <dmitry.shepelev@citeck.ru>
2019-01-30 17:09:17 +0400

    добавил возможность принудительно указать, куда проксировать трафик

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z "${PROXY_TARGET}" ]; then
-    sed -i "s,/community,${PROXY_TARGET},g" /etc/nginx/default.d/ssl
+if [ ! -z "${PROXY_TARGET}" ]; then
+    sed -i "s,community,${PROXY_TARGET},g" /etc/nginx/default.d/ssl
 fi
 nginx -g "daemon off;";
