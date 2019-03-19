0b700807a6b71964743a7c9231e63fa3f69e1ae9
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-03-19 18:05:50 +0300

    ECOSCOM-2050 - ECOS forms: SelectJournal control
    Update DatePicker; Handle datetime, int, long, float, double types

--- src/components/common/form/SelectJournal/predicates.js
+++ src/components/common/form/SelectJournal/predicates.js
@@ -7,14 +7,14 @@ import { t } from '../../../../helpers/util';
 export const COLUMN_DATA_TYPE_TEXT = 'text'; // +
 export const COLUMN_DATA_TYPE_MLTEXT = 'mltext'; // +
 export const COLUMN_DATA_TYPE_DATE = 'date'; // +
-export const COLUMN_DATA_TYPE_DATETIME = 'datetime'; // +- !!!
+export const COLUMN_DATA_TYPE_DATETIME = 'datetime'; // +
 export const COLUMN_DATA_TYPE_ASSOC = 'assoc'; // +
 export const COLUMN_DATA_TYPE_CATEGORY = 'category'; // +
 export const COLUMN_DATA_TYPE_CONTENT = 'content';
-export const COLUMN_DATA_TYPE_INT = 'int';
-export const COLUMN_DATA_TYPE_LONG = 'long';
-export const COLUMN_DATA_TYPE_FLOAT = 'float';
-export const COLUMN_DATA_TYPE_DOUBLE = 'double';
+export const COLUMN_DATA_TYPE_INT = 'int'; // +-
+export const COLUMN_DATA_TYPE_LONG = 'long'; // +-
+export const COLUMN_DATA_TYPE_FLOAT = 'float'; // +-
+export const COLUMN_DATA_TYPE_DOUBLE = 'double'; // +-
 export const COLUMN_DATA_TYPE_BOOLEAN = 'boolean';
 export const COLUMN_DATA_TYPE_QNAME = 'qname';
 export const COLUMN_DATA_TYPE_NODEREF = 'noderef';
@@ -54,7 +54,6 @@ const getAllPredicates = function() {
   ];
 };
 
-// TODO configure
 const PREDICATE_LIST_TYPE_STRING = [
   PREDICATE_CONTAINS,
   PREDICATE_EQ,
@@ -66,6 +65,9 @@ const PREDICATE_LIST_TYPE_STRING = [
 ];
 const PREDICATE_LIST_TYPE_DATE = [PREDICATE_GE, PREDICATE_LT, PREDICATE_EMPTY, PREDICATE_NOT_EMPTY];
 const PREDICATE_LIST_TYPE_NODE_REF = [PREDICATE_CONTAINS, PREDICATE_NOT_CONTAINS, PREDICATE_EMPTY, PREDICATE_NOT_EMPTY];
+const PREDICATE_LIST_TYPE_NUMBER = [PREDICATE_EQ, PREDICATE_NOT_EQ, PREDICATE_LT, PREDICATE_LE, PREDICATE_GT, PREDICATE_GE];
+const PREDICATE_LIST_TYPE_BOOLEAN = [PREDICATE_EMPTY, PREDICATE_NOT_EMPTY];
+const PREDICATE_LIST_TYPE_NO_CONTROL_YET = [PREDICATE_EMPTY, PREDICATE_NOT_EMPTY];
 
 let allPredicates = [];
 function filterPredicates(filterArr) {
@@ -78,16 +80,29 @@ function filterPredicates(filterArr) {
 
 export function getPredicates(field) {
   switch (field.type) {
+    case COLUMN_DATA_TYPE_BOOLEAN:
+      return filterPredicates(PREDICATE_LIST_TYPE_BOOLEAN);
+
+    case COLUMN_DATA_TYPE_INT:
+    case COLUMN_DATA_TYPE_DOUBLE:
+    case COLUMN_DATA_TYPE_LONG:
+    case COLUMN_DATA_TYPE_FLOAT:
+      return filterPredicates(PREDICATE_LIST_TYPE_NUMBER);
+
     case COLUMN_DATA_TYPE_ASSOC:
       return filterPredicates(PREDICATE_LIST_TYPE_NODE_REF);
+
     case COLUMN_DATA_TYPE_DATE:
     case COLUMN_DATA_TYPE_DATETIME:
       return filterPredicates(PREDICATE_LIST_TYPE_DATE);
+
     case COLUMN_DATA_TYPE_MLTEXT:
     case COLUMN_DATA_TYPE_TEXT:
     case COLUMN_DATA_TYPE_CATEGORY:
-    default:
       return filterPredicates(PREDICATE_LIST_TYPE_STRING);
+
+    default:
+      return filterPredicates(PREDICATE_LIST_TYPE_NO_CONTROL_YET);
   }
 }
 
@@ -104,9 +119,16 @@ export function getPredicateInput(field) {
           selected: predicateValue,
           onChange: function(value) {
             changePredicateValue(value);
-          }
+          },
+          showTimeInput: field.type === COLUMN_DATA_TYPE_DATETIME
         })
       };
+    case COLUMN_DATA_TYPE_INT:
+    case COLUMN_DATA_TYPE_DOUBLE:
+    case COLUMN_DATA_TYPE_LONG:
+    case COLUMN_DATA_TYPE_FLOAT:
+    // TODO use input type number
+    /* eslint-disable-next-line */
     case COLUMN_DATA_TYPE_MLTEXT:
     case COLUMN_DATA_TYPE_TEXT:
     case COLUMN_DATA_TYPE_CATEGORY:
@@ -126,19 +148,21 @@ export function getPredicateInput(field) {
           }
         })
       };
-    case COLUMN_DATA_TYPE_ASSOC: // TODO
-      // return {
-      //   component: Select,
-      //   defaultValue: null,
-      //   getProps: ({ predicateValue, changePredicateValue }) => ({
-      //     className: 'select_narrow',
-      //     loadOptions: () => {console.log('loadOptions')}
-      //     // value: predicateValue,
-      //     // onChange: function(e) {
-      //     //   changePredicateValue(e.target.value)
-      //     // },
-      //   }),
-      // };
+    case COLUMN_DATA_TYPE_OPTIONS:
+      return null;
+    // return {
+    //   component: Select,
+    //   defaultValue: null,
+    //   getProps: ({ predicateValue, changePredicateValue }) => ({
+    //     className: 'select_narrow',
+    //     loadOptions: () => {console.log('loadOptions')}
+    //     // value: predicateValue,
+    //     // onChange: function(e) {
+    //     //   changePredicateValue(e.target.value)
+    //     // },
+    //   }),
+    // };
+    case COLUMN_DATA_TYPE_ASSOC:
       return {
         component: SelectJournal,
         defaultValue: null,
