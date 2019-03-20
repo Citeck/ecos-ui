b9847ce831664bb71d8182dbb97202ded7272531
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-03-20 15:39:50 +0300

    ECOSCOM-2050 - ECOS forms: SelectJournal control
    register DatePicker locale

--- export/src/forms/index.js
+++ export/src/forms/index.js
@@ -2,6 +2,18 @@ import '../../../src/forms/style.scss';
 import customFormComponents from '../../../src/forms/components';
 import { linkBuilderEditForms } from '../../../src/forms/components/builder';
 
+import { registerLocale, setDefaultLocale } from 'react-datepicker';
+import datePickerLocaleEn from 'date-fns/locale/en-GB';
+import datePickerLocaleRu from 'date-fns/locale/ru';
+import { getCurrentLocale } from '../../../src/helpers/util';
+
+/* set DatePicker locale */
+registerLocale('en', datePickerLocaleEn);
+registerLocale('ru', datePickerLocaleRu);
+
+const currentLocale = getCurrentLocale();
+setDefaultLocale(currentLocale);
+
 // TODO export linkEditForms
 linkBuilderEditForms(customFormComponents);
 
