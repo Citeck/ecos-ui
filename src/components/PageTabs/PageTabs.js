5b9bc26ab01e66ee16056073811df97de80a4294
Nepritimov Maxim <mnepritimov@mmtr.ru>
2019-04-25 17:06:30 +0300

    ECOSENT-692 - Page tabs
    style for tabs complete, remove action complete, change url by click on tab is working

--- src/components/PageTabs/PageTabs.js
+++ src/components/PageTabs/PageTabs.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import './style.scss';
 
 class PageTabs extends React.Component {
   static propTypes = {
     children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    }),
     tabs: PropTypes.array
   };
 
@@ -17,47 +21,131 @@ class PageTabs extends React.Component {
         isActive: true,
         link: '/',
         title: 'Домашняя страница'
+      },
+      {
+        id: 'secondtab',
+        position: 1,
+        isActive: false,
+        link: '/share/page/journals',
+        title: 'Вторая страница с очень длинным названием, прям вот таким'
       }
     ]
   };
 
+  state = {
+    // ToDO: use tabs from props or from store
+    tabs: [
+      {
+        id: 'firsttab',
+        position: 0,
+        isActive: true,
+        link: '/share/page/journals',
+        title: 'Домашняя страница'
+      },
+      {
+        id: 'secondtab',
+        position: 1,
+        isActive: false,
+        link: '/formio-develop',
+        title: 'Вторая страница с очень длинным названием, прям вот таким'
+      },
+      {
+        id: '123',
+        position: 2,
+        isActive: false,
+        link: '/share/page/journalsDashboard',
+        title: '/share/page/journalsDashboard'
+      }
+    ]
+  };
+
+  handleCloseTab(tabId, event) {
+    event.stopPropagation();
+
+    const { tabs } = this.state;
+    const index = tabs.findIndex(tab => tab.id === tabId);
+
+    if (index === -1) {
+      return false;
+    }
+
+    let newTabs = [...tabs];
+
+    newTabs.splice(index, 1);
+
+    this.setState({
+      tabs: newTabs
+    });
+  }
+
+  handleSetActiveTab(tab) {
+    const { history } = this.props;
+    const { tabs } = this.state;
+
+    tabs.map(item => {
+      item.isActive = item.id === tab.id;
+
+      return item;
+    });
+
+    this.setState({ tabs }, () => {
+      history.push(tab.link);
+    });
+  }
+
   renderLeftButton() {
     return (
-      <div className="ecos-tab__nav-btn">
-        <div className="ecos-tab__nav-btn-icon icon-left" />
+      <div className="page-tab__nav-btn">
+        <div className="page-tab__nav-btn-icon icon-left" />
       </div>
     );
   }
 
   renderRightButton() {
     return (
-      <div className="ecos-tab__nav-btn ecos-tab__nav-btn_disable">
-        <div className="ecos-tab__nav-btn-icon icon-right" />
+      <div className="page-tab__nav-btn page-tab__nav-btn_disable">
+        <div className="page-tab__nav-btn-icon icon-right" />
       </div>
     );
   }
 
-  renderTabs() {
-    const { tabs } = this.props;
+  renderTabItem = item => {
+    const { history } = this.props;
+    const { tabs } = this.state;
+    const className = ['page-tab__tabs-item'];
+    const closeButton =
+      tabs.length > 1 ? <div className="page-tab__tabs-item-close icon-close" onClick={this.handleCloseTab.bind(this, item.id)} /> : null;
+
+    if (item.isActive) {
+      className.push('page-tab__tabs-item_active');
+    }
 
     return (
-      <div className="ecos-tab__tabs">
-        {tabs.map(item => (
-          <div key={item.id} className="ecos-tab__tabs-item" title={item.title}>
-            {item.title}
-          </div>
-        ))}
+      <div key={item.id} className={className.join(' ')} title={item.title} onClick={this.handleSetActiveTab.bind(this, item)}>
+        <span className="page-tab__tabs-item-title">{item.title}</span>
+        {closeButton}
+      </div>
+    );
+  };
+
+  renderTabs() {
+    const { tabs } = this.state;
+
+    return (
+      <div className="page-tab__tabs">
+        {tabs.map(this.renderTabItem)}
+        <div className="page-tab__tabs-add icon-plus" />
       </div>
     );
   }
 
   renderTabWrapper() {
-    if (!this.props.tabs.length) {
+    if (!this.state.tabs.length) {
       return null;
     }
 
     return (
-      <div className="ecos-tab">
+      <div className="page-tab">
         {this.renderLeftButton()}
         {this.renderTabs()}
         {this.renderRightButton()}
@@ -71,10 +159,10 @@ class PageTabs extends React.Component {
     return (
       <React.Fragment>
         {this.renderTabWrapper()}
-        {children}
+        <div className="page-tab__body">{children}</div>
       </React.Fragment>
     );
   }
 }
 
-export default PageTabs;
+export default withRouter(PageTabs);
