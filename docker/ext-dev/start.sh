9d60ee9400025452e654d58738ff6561c9ef14c6
Pavel Simonov <pavel.simonov@citeck.ru>
2019-04-29 12:43:13 +0700

    ECOSCOM-2161 - Migrate javascript and css from community to ecos-ui

--- docker/ext-dev/start.sh
+++ docker/ext-dev/start.sh
@@ -5,8 +5,7 @@ if [ ! -z "${PROXY_TARGET}" ]; then
     sed -i "s,ALFRESCO_TARGET,${PROXY_TARGET},g" /etc/nginx/conf.d/default.conf
 fi
 
-echo "Start nginx";
+echo "Start nginx. URL: http://localhost/share";
 
 nginx -g "daemon off;";
 
-echo "Nginx started";
