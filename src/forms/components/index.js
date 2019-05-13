3927b579b3cb9cfaf1367d33b9355747373f791b
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-05-13 12:51:32 +0300

    ECOSCOM-2082 - Add view mode support to the ECOS forms
    Checkbox

--- src/forms/components/index.js
+++ src/forms/components/index.js
@@ -1,13 +1,17 @@
+import DefaultComponents from 'formiojs/components';
+import Components from 'formiojs/components/Components';
+
+import Checkbox from './override/checkbox';
+
 import SelectJournal from './custom/selectJournal';
 import SelectOrgstruct from './custom/selectOrgstruct';
 import EcosSelect from './custom/ecosSelect';
 import AsyncData from './custom/asyncData';
 
-import DefaultComponents from 'formiojs/components';
-import Components from 'formiojs/components/Components';
-
 Components.setComponents({
   ...DefaultComponents,
+  checkbox: Checkbox,
+
   selectJournal: SelectJournal,
   selectOrgstruct: SelectOrgstruct,
   ecosSelect: EcosSelect,
