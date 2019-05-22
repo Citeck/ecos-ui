cd00cbc88c4b4c8384d51c21d8c0bac4286360ea
atupikova <atupikova@mmtr.ru>
2019-05-22 12:46:46 +0300

    ECOSENT-711 start: route, container

---
+++ src/pages/LayoutSettingsPage/LayoutSettingsPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({});
+
+class LayoutSettingsPage extends React.Component {
+  render() {
+    return <div>тест</div>;
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LayoutSettingsPage);
