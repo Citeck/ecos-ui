d1e84da839b795f8bf47a8121dd0a35e46569eda
atupikova <atupikova@mmtr.ru>
2019-05-24 13:49:52 +0300

    ECOSENT-711 DragItem & SelectWidgets

--- src/components/SelectWidgets/SelectWidgets.js
+++ src/components/SelectWidgets/SelectWidgets.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import classNames from 'classnames';
 import { Scrollbars } from 'react-custom-scrollbars';
+import './style.scss';
 import DragItem from '../DragItem';
 
-import './style.scss';
-
 class SelectWidgets extends React.Component {
+  _className = 'ecos-select-widgets';
+
+  renderView = props => <div {...props} className={classNames(`${this._className}__scrollbar`)} />;
+  renderTrackHorizontal = props => <div {...props} hidden />;
+
   render() {
-    let baseClass = 'ecos-select-widgets';
-    const renderView = props => <div {...props} className={classNames(`${baseClass}__scrollbar`)} />;
-    const renderTrackHorizontal = props => <div {...props} hidden />;
+    const arr = new Array(50);
+    arr.fill('item');
 
     return (
-      <div className={`${baseClass}`}>
-        <Scrollbars renderView={renderView} renderTrackHorizontal={renderTrackHorizontal}>
-          <DragItem />
-          <DragItem />
-          <DragItem />
-          <DragItem />
+      <div className={`${this._className}`}>
+        <Scrollbars renderView={this.renderView} renderTrackHorizontal={this.renderTrackHorizontal}>
+          {arr.map(item => (
+            <DragItem title={item} />
+          ))}
         </Scrollbars>
       </div>
     );
