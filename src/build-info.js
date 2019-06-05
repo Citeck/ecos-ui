dea6bbbad637095284bbd3bdb0999b2b326b0e16
Pavel Simonov <pavel.simonov@citeck.ru>
2019-06-05 14:15:53 +0700

    ECOSCOM-2235 - Add left menu request caching

--- src/build-info.js
+++ src/build-info.js
@@ -2,7 +2,7 @@ import preval from 'preval.macro';
 import packageJson from '../package.json';
 
 const info = {
-  timestamp: preval`module.exports = new Date().toLocaleString()`,
+  time: preval`module.exports = new Date().toLocaleString()`,
   version: packageJson.version
 };
 
