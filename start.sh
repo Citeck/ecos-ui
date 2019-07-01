6ba1db9245f6bd572fcfcf0c90561d9de4a0beca
sergei smagin <sergey.smagin@citeck.ru>
2019-07-01 10:16:10 +0300

    В docker-compose.yaml необходимо указать переменную GATEWAY_TARGET в ней записываем адрес сервиса gateway(или имя контейнера) и слушающий порт. Дефолтное значение gateway:8085 Также теперь необходимо в docker-compose.yaml записывать порт сервиса ecos в переменную PROXY_TARGET было: PROXY_TARGET=ecos стало: PROXY_TARGET=ecos:8080
    Скрипт start.sh проверяет переменные и подставляет их значения в секции соответствующих апстримов.
    Если сервис gateway отсутствует, переменную GATEWAY_TARGET оставляем пустой и этот апстрим будет удален из конфигурационного файла nginx.

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ ! -z "${PROXY_TARGET}" ]; then
-    sed -i "s,community,${PROXY_TARGET},g" /etc/nginx/conf.d/default.conf
+if [ -n "${PROXY_TARGET}" ]; then
+    sed -i "s,ALFRESCO_TARGET,${PROXY_TARGET},g" /etc/nginx/conf.d/default.conf
+fi
+if [ -z "${GATEWAY_TARGET}" ]; then
+    sed -i '/#BEGIN_GATEWAY/,/#END_GATEWAY/d' /etc/nginx/conf.d/default.conf
+else
+    sed -i "s/GATEWAY_TARGET/${GATEWAY_TARGET}/" /etc/nginx/conf.d/default.conf
 fi
 nginx -g "daemon off;";
+
