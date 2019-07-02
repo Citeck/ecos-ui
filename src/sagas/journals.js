5ee646c661162031b6288fd08397d396bee0185d
Andrienko <malig33@mail.ru>
2019-07-02 12:56:18 +0700

    ECOSCOM-2295 - Bugs of new journal  * download zip

--- src/sagas/journals.js
+++ src/sagas/journals.js
@@ -33,7 +33,9 @@ import {
   goToJournalsPage,
   search,
   performGroupAction,
-  setPerformGroupActionResponse
+  setPerformGroupActionResponse,
+  createZip,
+  setZipNodeRef
 } from '../actions/journals';
 import { setLoading } from '../actions/loader';
 import { JOURNAL_SETTING_ID_FIELD, DEFAULT_PAGINATION } from '../components/Journals/constants';
@@ -472,6 +474,21 @@ function* sagaPerformGroupAction({ api, logger, stateId, w }, action) {
   }
 }
 
+function* sagaCreateZip({ api, logger, stateId, w }, action) {
+  try {
+    let selected = action.payload;
+
+    const nodeRef = yield call(api.journals.createZip, selected);
+
+    if (nodeRef) {
+      yield call(api.journals.deleteDownloadsProgress, nodeRef);
+      yield put(setZipNodeRef(w(nodeRef)));
+    }
+  } catch (e) {
+    logger.error('[journals sagaCreateZip saga error', e.message);
+  }
+}
+
 function* saga(ea) {
   yield takeEvery(getDashletConfig().type, wrapSaga, { ...ea, saga: sagaGetDashletConfig });
   yield takeEvery(getDashletEditorData().type, wrapSaga, { ...ea, saga: sagaGetDashletEditorData });
@@ -497,6 +514,7 @@ function* saga(ea) {
   yield takeEvery(goToJournalsPage().type, wrapSaga, { ...ea, saga: sagaGoToJournalsPage });
   yield takeEvery(search().type, wrapSaga, { ...ea, saga: sagaSearch });
   yield takeEvery(performGroupAction().type, wrapSaga, { ...ea, saga: sagaPerformGroupAction });
+  yield takeEvery(createZip().type, wrapSaga, { ...ea, saga: sagaCreateZip });
 }
 
 export default saga;
