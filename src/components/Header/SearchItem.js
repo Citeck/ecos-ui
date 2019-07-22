2a4f5864589b7fa47a34291a8310ffb3e1a987a8
atupikova <atupikova@mmtr.ru>
2019-07-22 18:09:35 +0300

    ECOSENT-776 fixed style search item

--- src/components/Header/SearchItem.js
+++ src/components/Header/SearchItem.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import { Avatar, Icon, Separator } from '../common';
+import { Avatar, Icon } from '../common';
 
 export default class SearchItem extends React.PureComponent {
   static propTypes = {
@@ -29,24 +30,22 @@ export default class SearchItem extends React.PureComponent {
   render() {
     const { data } = this.props;
     const { icon, title, description, groupName, avatarUrl, isLast, isAvatar } = data || {};
+    const cssContent = `${this.className}__content`;
 
     return groupName ? (
       <li className={`${this.className}__group-name`}>{groupName}</li>
     ) : (
       <React.Fragment>
         <li onClick={this.onClick} className={this.className}>
-          {icon && <Icon className={`${icon} ${this.className}__icon`} />}
-          {isAvatar && <Avatar url={avatarUrl} className={`${this.className}__avatar`} />}
-          <div className={`${this.className}__data`}>
-            <div className={`${this.className}__title`}>{title}</div>
-            <div className={`${this.className}__desc`}>{description}</div>
+          <div className={classNames(cssContent)}>
+            {icon && <Icon className={`${icon} ${cssContent}-icon`} />}
+            {isAvatar && <Avatar url={avatarUrl} className={`${cssContent}-avatar`} />}
+            <div className={`${cssContent}-data`}>
+              <div className={`${cssContent}-title`}>{title}</div>
+              <div className={`${cssContent}-desc`}>{description}</div>
+            </div>
           </div>
         </li>
-        {!isLast && (
-          <div className={`${this.className}__line`}>
-            <Separator noIndents />
-          </div>
-        )}
       </React.Fragment>
     );
   }
