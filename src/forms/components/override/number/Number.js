58df201f70630a72520d37188db67ce03a66e203
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-07-23 13:51:12 +0300

    ECOSCOM-2383 - Incorrect Amount

---
+++ src/forms/components/override/number/Number.js
@@ -0,0 +1,20 @@
+import FormIONumberComponent from 'formiojs/components/number/Number';
+import _ from 'lodash';
+
+export default class NumberComponent extends FormIONumberComponent {
+  getMaskedValue(value) {
+    return this.formatValue(this.clearInput(value));
+  }
+
+  formatValue(value) {
+    const decimalLimit = _.get(this.component, 'decimalLimit', this.decimalLimit);
+
+    if (this.component.requireDecimal && value && !value.includes(this.decimalSeparator)) {
+      return `${value}${this.decimalSeparator}${_.repeat('0', decimalLimit)}`;
+    } else if (this.component.requireDecimal && value && value.includes(this.decimalSeparator)) {
+      return `${value}${_.repeat('0', decimalLimit - value.split(this.decimalSeparator)[1].length)}`;
+    }
+
+    return value;
+  }
+}
