2a0cb55793c76a593a4181fe923208d2c27c0d81
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-07-25 12:20:03 +0300

    ECOSENT-776 - Improve top menu
    added hamburger icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 import ReactResizeDetector from 'react-resize-detector';
 import { fetchCreateCaseWidgetData, fetchSiteMenuData, fetchUserMenuData } from '../../actions/header';
 import CreateMenu from './CreateMenu';
+import HamburgerIcon from './HamburgerIcon';
 import UserMenu from './UserMenu';
 import SiteMenu from './SiteMenu';
 import Search from './Search';
@@ -66,6 +67,7 @@ class Header extends React.Component {
         <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} />
         <div className={classNameContainer}>
           <div className={`${classNameSide} ${classNameSide}_left`}>
+            <HamburgerIcon />
             <CreateMenu isMobile={widthHeader < 910} />
           </div>
           <div className={`${classNameSide} ${classNameSide}_right`}>
