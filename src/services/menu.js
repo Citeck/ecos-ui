c13ba621e9b1e89ff68e88d07f93aef35b748d6a
Nepritimov Maxim <mnepritimov@mmtr.ru>
2019-07-26 13:19:25 +0300

    ECOSCOM-2395 - fix generate dashboard settings url

--- src/services/menu.js
+++ src/services/menu.js
@@ -1,15 +1,23 @@
 import { select } from 'redux-saga/effects';
 import { selectIdentificationForView } from '../selectors/dashboard';
+import { getSearchParams } from '../helpers/urls';
 
 export default class MenuService {
   static processTransitSiteMenuItem = function*(menuItem) {
     const dashboard = yield select(selectIdentificationForView);
-
+    const { recordRef } = getSearchParams();
+    const params = [];
     let link = menuItem.targetUrl;
 
     if (menuItem.id === 'SETTINGS_HOME_PAGE') {
-      link += '?dashboardId=' + dashboard.id;
+      params.push(`dashboardId=${dashboard.id}`);
+
+      if (recordRef) {
+        params.push(`recordRef=${recordRef}`);
+      }
     }
+
+    link += `?${params.join('&')}`;
 
     return link;
   };
