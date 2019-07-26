f25009404069ae4ad0e8804c69eac48c62205a5a
Nepritimov Maxim <mnepritimov@mmtr.ru>
2019-07-26 14:53:38 +0300

    ECOSENT-795 - clean dashboard data when changed url

--- src/actions/dashboard.js
+++ src/actions/dashboard.js
@@ -9,3 +9,4 @@ export const setDashboardTitleInfo = createAction(prefix + 'SET_TITLE_INFO');
 
 export const getDashboardConfig = createAction(prefix + 'GET_DASHBOARD_CONFIG');
 export const saveDashboardConfig = createAction(prefix + 'SAVE_DASHBOARD_CONFIG');
+export const resetDashboardConfig = createAction(prefix + 'RESET_DASHBOARD_CONFIG');
