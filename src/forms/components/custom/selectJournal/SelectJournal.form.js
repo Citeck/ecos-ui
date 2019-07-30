4b2e45012baf81d2912d29ffe7e475eff1f83229
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-07-30 11:12:46 +0300

    ECOSCOM-2405 - ECOS Forms. Opportunity of configurating Select Journal Component.

--- src/forms/components/custom/selectJournal/SelectJournal.form.js
+++ src/forms/components/custom/selectJournal/SelectJournal.form.js
@@ -1,9 +1,14 @@
 import baseEditForm from 'formiojs/components/base/Base.form';
+import SelectJournalEditDisplay from './editForm/SelectJournal.edit.display';
 import SelectJournalEditData from './editForm/SelectJournal.edit.data';
 
 export default function(...extend) {
   return baseEditForm(
     [
+      {
+        key: 'display',
+        components: SelectJournalEditDisplay
+      },
       {
         key: 'data',
         components: SelectJournalEditData
