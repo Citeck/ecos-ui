a8584aa9262a6f24869a4352c39ad60be8692532
Nepritimov Maxim <mnepritimov@mmtr.ru>
2019-08-02 17:33:00 +0300

    ECOSENT-780 - division Timesheet into components

---
+++ src/components/Timesheet/BaseTimesheet.js
@@ -0,0 +1,202 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Scrollbars } from 'react-custom-scrollbars';
+import classNames from 'classnames';
+import { SortableContainer, SortableElement, SortableHandle } from '../Drag-n-Drop';
+import { Input } from '../common/form';
+import Hour from './Hour';
+import DayCell from './DayCell';
+import { t, deepClone } from '../../helpers/util';
+import './style.scss';
+
+class BaseTimesheet extends Component {
+  static propTypes = {
+    eventTypes: PropTypes.array,
+    daysOfMonth: PropTypes.array
+  };
+
+  static defaultProps = {
+    eventTypes: [],
+    daysOfMonth: []
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      typeFilter: '',
+      filteredEventTypes: deepClone(props.eventTypes)
+    };
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    if (JSON.stringify(nextProps.eventTypes) !== JSON.stringify(this.state.eventTypes)) {
+      this.setState({
+        eventTypes: nextProps.eventTypes,
+        filteredEventTypes: deepClone(nextProps.eventTypes)
+      });
+    }
+  }
+
+  getFiltered(eventTypes, filter) {
+    return eventTypes.filter(item => item.title.toLowerCase().includes(filter.toLowerCase()));
+  }
+
+  handleFilterTypes = event => {
+    this.filterTypes(event.target.value);
+  };
+
+  handleClearFilterTypes = () => {
+    this.filterTypes('');
+  };
+
+  handleSortEnd = ({ oldIndex, newIndex }, event) => {
+    const { filteredEventTypes } = this.state;
+    const eventTypes = deepClone(filteredEventTypes);
+    const draggableEvent = eventTypes[oldIndex];
+
+    event.stopPropagation();
+
+    eventTypes.splice(oldIndex, 1);
+    eventTypes.splice(newIndex, 0, draggableEvent);
+
+    this.setState({ filteredEventTypes: eventTypes });
+  };
+
+  filterTypes(typeFilter = '') {
+    const { eventTypes } = this.props;
+    let filteredEventTypes = deepClone(eventTypes);
+
+    if (typeFilter) {
+      filteredEventTypes = this.getFiltered(eventTypes, typeFilter);
+    }
+
+    this.setState({ typeFilter, filteredEventTypes });
+  }
+
+  renderFilter() {
+    const { typeFilter } = this.state;
+
+    return (
+      <div className={classNames('ecos-timesheet__table-search')}>
+        <Input
+          className="ecos-timesheet__table-search-input"
+          placeholder={t('Найти событие')}
+          value={typeFilter}
+          onChange={this.handleFilterTypes}
+        />
+
+        {typeFilter && <div className="ecos-timesheet__table-search-clear" onClick={this.handleClearFilterTypes} />}
+      </div>
+    );
+  }
+
+  renderEventTypes(eventTypes = this.state.filteredEventTypes, key = null) {
+    return (
+      <SortableContainer
+        axis="y"
+        lockAxis="y"
+        onSortEnd={key === null ? this.handleSortEnd : this.handleSortEndInGroup.bind(this, key)}
+        // updateBeforeSortStart={this.handleBeforeSortStart}
+        useDragHandle
+      >
+        <div className="ecos-timesheet__table-events">{eventTypes.map(this.renderEventType)}</div>
+      </SortableContainer>
+    );
+  }
+
+  renderEventType = (item, position) => {
+    return (
+      <SortableElement key={position} index={position}>
+        <div className="ecos-timesheet__table-events-item">
+          <SortableHandle>
+            <div className="ecos-timesheet__table-events-item-dnd" />
+          </SortableHandle>
+
+          <div className="ecos-timesheet__table-events-item-filter" style={{ backgroundColor: item.color || '#D0D0D0' }} />
+          <div className="ecos-timesheet__table-events-item-title">{item.title}</div>
+          <div className="ecos-timesheet__table-events-item-add-btn" />
+        </div>
+      </SortableElement>
+    );
+  };
+
+  renderCountByDay = day => (
+    <DayCell day={day} key={day.title}>
+      10
+    </DayCell>
+  );
+
+  renderCalendarHeader() {
+    const { daysOfMonth } = this.props;
+
+    return [
+      <div className="ecos-timesheet__table-calendar-row" key="date">
+        {daysOfMonth.map(day => (
+          <DayCell day={day} key={day.title}>
+            {day.title}
+          </DayCell>
+        ))}
+      </div>,
+      <div className="ecos-timesheet__table-calendar-row" key="hours">
+        {daysOfMonth.map(this.renderCountByDay)}
+      </div>
+    ];
+  }
+
+  renderEvents() {
+    const { filteredEventTypes } = this.state;
+
+    return filteredEventTypes.map(this.renderEventCalendarRow);
+  }
+
+  renderEventCalendarRow = (event, eventIndex) => (
+    <div className="ecos-timesheet__table-calendar-row" key={eventIndex}>
+      {this.props.daysOfMonth.map((day, dayIndex) => (
+        <div className="ecos-timesheet__table-calendar-cell" key={dayIndex}>
+          <div className="ecos-timesheet__table-calendar-cell-content">
+            <Hour
+              key={`${eventIndex}-${event.name}-${day.title}-${dayIndex}`}
+              color={event.color}
+              count={Math.round(Math.random())}
+              canEdit={event.canEdit}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
+  renderCalendar() {
+    return (
+      <Scrollbars autoHeight autoHeightMin={40} autoHeightMax={'100%'} renderThumbVertical={props => <div {...props} hidden />}>
+        <div className="ecos-timesheet__table-calendar">
+          {this.renderCalendarHeader()}
+          {this.renderEvents()}
+        </div>
+      </Scrollbars>
+    );
+  }
+
+  renderDay = day => {
+    return (
+      <div key={day.number} className="ecos-timesheet__table-calendar-item">
+        <div className="ecos-timesheet__table-calendar-day">{day.title}</div>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div className="ecos-timesheet__table">
+        <div className="ecos-timesheet__table-left-column">
+          {this.renderFilter()}
+          {this.renderEventTypes()}
+        </div>
+        <div className="ecos-timesheet__table-right-column">{this.renderCalendar()}</div>
+      </div>
+    );
+  }
+}
+
+export default BaseTimesheet;
