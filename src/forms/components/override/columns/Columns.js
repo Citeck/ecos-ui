5822632700cb79adcd55953c74d0bd1dea80a60d
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-09-05 18:13:48 +0300

    ECOSCOM-2502 - ECOS Forms. Can't create "Columns Component" with 1 column.

--- src/forms/components/override/columns/Columns.js
+++ src/forms/components/override/columns/Columns.js
@@ -1,5 +1,6 @@
 import NestedComponent from 'formiojs/components/nested/NestedComponent';
 import FormIOColumnsComponent from 'formiojs/components/columns/Columns';
+import _ from 'lodash';
 
 export default class ColumnsComponent extends FormIOColumnsComponent {
   static schema(...extend) {
@@ -8,12 +9,7 @@ export default class ColumnsComponent extends FormIOColumnsComponent {
         label: 'Columns',
         key: 'columns',
         type: 'columns',
-        columns: [
-          { components: [], classes: '' },
-          { components: [], classes: '' }
-          // { components: [], xs: 0, sm: 12, md: 6, lg: 0, xl: 0, classes: '' },
-          // { components: [], xs: 0, sm: 12, md: 6, lg: 0, xl: 0, classes: '' }
-        ],
+        columns: [{ components: [], xs: 0, sm: 12, md: 6, lg: 0, xl: 0, classes: '' }],
         clearOnHide: false,
         input: false,
         tableView: false,
@@ -23,6 +19,48 @@ export default class ColumnsComponent extends FormIOColumnsComponent {
       },
       ...extend
     );
+  }
+
+  static get builderInfo() {
+    return {
+      ...super.builderInfo,
+      schema: ColumnsComponent.schema()
+    };
+  }
+
+  get defaultSchema() {
+    return ColumnsComponent.schema();
+  }
+
+  // TODO delete when update formiojs to v4
+  get schema() {
+    const superSchema = Object.getOwnPropertyDescriptor(NestedComponent.prototype, 'schema').get.call(this);
+    const schema = _.omit(superSchema, 'components');
+
+    const saveIndices = schema.columns.map(item => item.index);
+    const columnsNewLength = schema.columns.length;
+
+    schema.columns = [];
+    let lastIdx = 0;
+    this.eachComponent((component, index) => {
+      _.merge(component.component, _.omit(this.component.columns[index], 'components'));
+      schema.columns.push({ ...component.schema, index: lastIdx++ });
+    });
+    for (let i = this.components.length; i < this.component.columns.length; i++) {
+      schema.columns.push({ ...this.component.columns[i], index: lastIdx++ });
+    }
+
+    if (columnsNewLength < schema.columns.length) {
+      schema.columns = schema.columns
+        .filter(item => {
+          return saveIndices.some(index => item.index === index);
+        })
+        .map((item, index) => {
+          return { ...item, index };
+        });
+    }
+
+    return schema;
   }
 
   get className() {
