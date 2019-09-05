48bd1c758838637f2816dcd0100cfa77d15074e0
atupikova <ann_040294@mail.ru>
2019-09-05 18:47:28 +0300

    ECOSENT-901 refactoring Caption; update title Tasks

--- src/components/common/form/Caption/Caption.jsx
+++ src/components/common/form/Caption/Caption.jsx
@@ -6,15 +6,20 @@ import './Caption.scss';
 
 class Caption extends Component {
   render() {
-    const { children, className, extra, large, middle } = { ...this.props };
+    const { children, className, extra, large, middle, small } = { ...this.props };
 
-    return extra ? (
-      <h1 className={classNames('ecos-caption ecos-caption_extra', className)}>{children}</h1>
-    ) : large ? (
-      <h2 className={classNames('ecos-caption ecos-caption_large', className)}>{children}</h2>
-    ) : middle ? (
-      <h3 className={classNames('ecos-caption ecos-caption_middle', className)}>{children}</h3>
-    ) : null;
+    switch (true) {
+      case extra:
+        return <h1 className={classNames('ecos-caption ecos-caption_extra', className)}>{children}</h1>;
+      case large:
+        return <h2 className={classNames('ecos-caption ecos-caption_large', className)}>{children}</h2>;
+      case middle:
+        return <h3 className={classNames('ecos-caption ecos-caption_middle', className)}>{children}</h3>;
+      case small:
+        return <h4 className={classNames('ecos-caption ecos-caption_small', className)}>{children}</h4>;
+      default:
+        return <div className={classNames(className)}>{children}</div>;
+    }
   }
 }
 
@@ -22,14 +27,16 @@ Caption.propTypes = {
   className: PropTypes.string,
   extra: PropTypes.bool,
   large: PropTypes.bool,
-  middle: PropTypes.bool
+  middle: PropTypes.bool,
+  small: PropTypes.bool
 };
 
 Caption.defaultProps = {
   className: '',
   extra: false,
   large: false,
-  middle: false
+  middle: false,
+  small: false
 };
 
 export default Caption;
