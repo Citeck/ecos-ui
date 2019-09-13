26d0541a8c5a0abc1a9f14f784fcfa15613269d7
atupikova <ann_040294@mail.ru>
2019-09-13 09:12:12 +0300

    Revert "ECOSENT-916 new frame for content, styles for menu, refactoring"
    This reverts commit 831508ea

--- src/components/SlideMenu/SlideMenu.js
+++ src/components/SlideMenu/SlideMenu.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import LogoBlock from './LogoBlock';
 import RootList from './RootList';
+import './slide-menu.css';
 import { fetchLargeLogoSrc, fetchSlideMenuItems, fetchSmallLogoSrc, toggleIsOpen } from '../../actions/slideMenu';
 import { t } from '../../helpers/util';
-
-import './style.scss';
 
 const mapStateToProps = state => ({
   isOpen: state.slideMenu.isOpen,
@@ -13,17 +13,60 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchSmallLogoSrc: () => dispatch(fetchSmallLogoSrc()),
-  fetchLargeLogoSrc: () => dispatch(fetchLargeLogoSrc()),
-  fetchSlideMenuItems: () => dispatch(fetchSlideMenuItems()),
+  fetchSmallLogoSrc: () => {
+    dispatch(fetchSmallLogoSrc());
+  },
+  fetchLargeLogoSrc: () => {
+    dispatch(fetchLargeLogoSrc());
+  },
+  fetchSlideMenuItems: () => {
+    dispatch(fetchSlideMenuItems());
+  },
   toggleIsOpen: isOpen => dispatch(toggleIsOpen(isOpen))
 });
 
 class SlideMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.checkbox = document.createElement('input');
+    this.checkbox.id = 'slide-menu-checkbox';
+    this.checkbox.type = 'checkbox';
+
+    this.menu = document.createElement('div');
+    this.menu.classList.add('slide-menu');
+
+    this.mask = document.createElement('div');
+    this.mask.classList.add('slide-menu-mask');
+
+    const maskLabel = document.createElement('label');
+    maskLabel.setAttribute('for', 'slide-menu-checkbox');
+    this.mask.appendChild(maskLabel);
+  }
+
   componentDidMount() {
+    const theFirstChild = document.body.firstChild;
+    document.body.insertBefore(this.checkbox, theFirstChild);
+    document.body.insertBefore(this.menu, theFirstChild);
+    document.body.insertBefore(this.mask, theFirstChild);
+
+    this.checkbox.addEventListener('change', this.onCheckboxChange);
+
     this.props.fetchSmallLogoSrc();
     this.props.fetchLargeLogoSrc();
     this.props.fetchSlideMenuItems();
+  }
+
+  componentDidUpdate() {
+    this.checkbox.classList.add('slide-menu-checkbox_ready');
+  }
+
+  componentWillUnmount() {
+    this.checkbox.removeEventListener('change', this.onCheckboxChange);
+
+    document.body.removeChild(this.checkbox);
+    document.body.removeChild(this.menu);
+    document.body.removeChild(this.mask);
   }
 
   onCheckboxChange = e => {
@@ -43,8 +86,8 @@ class SlideMenu extends React.Component {
 
     const toggleHandleTitle = isOpen ? '' : t('slide_menu_click_to_open.label');
 
-    return (
-      <div className="slide-menu slide-menu_open">
+    return ReactDOM.createPortal(
+      <Fragment>
         <label
           ref={el => (this.slideMenuToggle = el)}
           className="slide-menu-toggle"
@@ -53,7 +96,8 @@ class SlideMenu extends React.Component {
         />
         <LogoBlock />
         <RootList toggleSlideMenu={this.toggleSlideMenu} />
-      </div>
+      </Fragment>,
+      this.menu
     );
   }
 }
