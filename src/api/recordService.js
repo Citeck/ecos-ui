988a0b2e2ee2b21302208ecccfccfe79c42282b6
Pavel Simonov <pavel.simonov@citeck.ru>
2019-09-19 13:30:20 +0700

    Mark old RecordService as deprecated

--- src/api/recordService.js
+++ src/api/recordService.js
@@ -1,28 +1,24 @@
 import { CommonApi } from './common';
 import { PROXY_URI } from '../constants/alfresco';
 
+/* DEPRECATED. Use Records.js instead */
+//todo: remove it
 export class RecordService extends CommonApi {
+  /* DEPRECATED */
   query = dataObj => {
     const url = `${PROXY_URI}citeck/ecos/records/query`;
     return this.postJson(url, dataObj);
   };
 
+  /* DEPRECATED */
   mutate = dataObj => {
     const url = `${PROXY_URI}citeck/ecos/records/mutate`;
     return this.postJson(url, dataObj);
   };
 
+  /* DEPRECATED */
   delete = dataObj => {
     const url = `${PROXY_URI}citeck/ecos/records/delete`;
     return this.postJson(url, dataObj);
-  };
-
-  getRecordsDisplayName = (records = []) => {
-    return this.query({
-      records: records,
-      attributes: {
-        name: '.disp'
-      }
-    });
   };
 }
