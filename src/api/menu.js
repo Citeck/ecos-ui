c29bc0a35f6c3340355f71e03fcc26b8729a7a6e
atupikova <ann_040294@mail.ru>
2019-10-02 15:25:44 +0300

    ECOSENT-963 option link to enable or disable section

--- src/api/menu.js
+++ src/api/menu.js
@@ -114,4 +114,8 @@ export class MenuApi extends CommonApi {
 
     return record.save().then(resp => resp);
   };
+
+  checkSiteDashboardEnable = () => {
+    return Records.get('uiserv/config@site-dashboard-enable').load('value?bool');
+  };
 }
