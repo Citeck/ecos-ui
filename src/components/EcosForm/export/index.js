8c534b6095d2d66c4b9353cf14b4845ba5dca36f
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-10-07 13:49:22 +0300

    ECOSCOM-2727 - Incorrect color of header at journal widget

--- src/components/EcosForm/export/index.js
+++ src/components/EcosForm/export/index.js
@@ -1,6 +1,3 @@
-import '../../../forms/style.scss';
-import '../../../forms/components';
-
 import { registerLocale, setDefaultLocale } from 'react-datepicker';
 import datePickerLocaleEn from 'date-fns/locale/en-GB';
 import datePickerLocaleRu from 'date-fns/locale/ru';
@@ -12,7 +9,5 @@ registerLocale('ru', datePickerLocaleRu);
 
 const currentLocale = getCurrentLocale();
 setDefaultLocale(currentLocale);
-
-//TODO: make normal component
 
 export { default } from '../EcosForm';
