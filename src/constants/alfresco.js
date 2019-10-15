c23676cce1cd19ba1d0c038e651edd9c1739100a
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-10-15 11:12:48 +0300

    ECOSCOM-2747 - Remove share javascript from ecos-ui

--- src/constants/alfresco.js
+++ src/constants/alfresco.js
@@ -7,3 +7,16 @@ export const URL_PAGECONTEXT = '/share/page/';
 export const URL_SERVICECONTEXT = '/share/service/';
 export const COOKIE_KEY_LOCALE = 'alf_share_locale';
 export const COOKIE_KEY_LOCALE_MAX_AGE = 30 * 24 * 60 * 60;
+
+window.Alfresco = window.Alfresco || {};
+window.Alfresco.constants = window.Alfresco.constants || {};
+window.Alfresco.constants = {
+  ...window.Alfresco.constants,
+  URL_CONTEXT,
+  PROXY_URI,
+  PROXY_URI_SHORT,
+  MICRO_URI,
+  URL_RESCONTEXT,
+  URL_PAGECONTEXT,
+  URL_SERVICECONTEXT
+};
