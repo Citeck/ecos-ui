83358fdf4c01f83f87ed1f05defb3c54e5f7f3ba
Nepritimov <skinet788767@gmail.com>
2019-10-19 21:31:30 +0300

    ECOSENT-988 - settings layout

--- src/components/WebPage/WebPage.js
+++ src/components/WebPage/WebPage.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import get from 'lodash/get';
 
 import { Btn } from '../common/btns';
+import { Label, Input } from '../common/form';
 import Dashlet from '../Dashlet/Dashlet';
 import { t } from '../../helpers/util';
 import './style.scss';
@@ -13,7 +14,11 @@ import { MIN_WIDTH_DASHLET_SMALL } from '../../constants';
 
 const LABELS = {
   EMPTY_DATA: 'Содержимого нет. Укажите в настройках адрес веб-страницы.',
-  SETTINGS_BTN: 'Настроить'
+  SETTINGS_BTN: 'Настроить',
+  SETTINGS_LABEL_TITLE: 'Заголовок виджета',
+  SETTINGS_PLACEHOLDER_TITLE: 'Например название веб-ресурса',
+  SETTINGS_LABEL_URL: 'URL-адрес ресурса',
+  SETTINGS_PLACEHOLDER_URL: 'www.google.com'
 };
 
 class WebPage extends Component {
@@ -35,12 +40,15 @@ class WebPage extends Component {
       width: MIN_WIDTH_DASHLET_SMALL,
       fitHeights: {},
       contentHeight: null,
+      settingsIsShow: false,
       userHeight: UserLocalSettingsService.getDashletHeight(props.id),
       isCollapsed: UserLocalSettingsService.getProperty(props.id, 'isCollapsed')
     };
   }
 
-  handleEdit = () => {};
+  handleEdit = () => {
+    this.setState({ settingsIsShow: true });
+  };
 
   handleReload = () => {};
 
@@ -55,8 +63,9 @@ class WebPage extends Component {
 
   renderEmptyData() {
     const { url } = this.props;
+    const { settingsIsShow } = this.state;
 
-    if (url.length) {
+    if (url || settingsIsShow) {
       return null;
     }
 
@@ -64,9 +73,31 @@ class WebPage extends Component {
       <div className="ecos-wpage__ground">
         <div className="ecos-wpage__text">{t(LABELS.EMPTY_DATA)}</div>
 
-        <Btn className="ecos-btn_blue ecos-btn_hover_light-blue">
+        <Btn className="ecos-btn_blue ecos-btn_hover_light-blue" onClick={this.handleEdit}>
           <span className="ecos-vj__btn-add-title">{t(LABELS.SETTINGS_BTN)}</span>
         </Btn>
+      </div>
+    );
+  }
+
+  renderSettings() {
+    const { settingsIsShow } = this.state;
+
+    if (!settingsIsShow) {
+      return null;
+    }
+
+    return (
+      <div className="ecos-wpage__settings">
+        <Label for="title-input" className="ecos-wpage__settings-label">
+          {t(LABELS.SETTINGS_LABEL_TITLE)}
+        </Label>
+        <Input id="title-input" className="ecos-wpage__settings-input" placeholder={t(LABELS.SETTINGS_PLACEHOLDER_TITLE)} />
+
+        <Label for="url-input" className="ecos-wpage__settings-label">
+          {t(LABELS.SETTINGS_LABEL_URL)}
+        </Label>
+        <Input id="url-input" className="ecos-wpage__settings-input" placeholder={t(LABELS.SETTINGS_PLACEHOLDER_URL)} />
       </div>
     );
   }
@@ -88,6 +119,7 @@ class WebPage extends Component {
         onReload={this.handleReload}
       >
         {this.renderEmptyData()}
+        {this.renderSettings()}
       </Dashlet>
     );
   }
