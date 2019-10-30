f8dffed486ca6b419d4ab6a412d8be5b5e39ee19
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-10-30 14:48:15 +0300

    ECOSCOM-2834 - Export slide menu from ecos-ui to share
    rename exports

--- export/src/exports.js
+++ export/src/exports.js
@@ -32,6 +32,6 @@ export default {
   'cardlet-node-view': () => import('../../src/legacy/cardlets/node-view/node-view'),
   'journalsApi': () => journalsApi,
   'header': () => import('../../src/components/Header/export'),
-  'slide-menu': () => import('../../src/components/SlideMenu/export'),
-  'legacy-slide-menu': () => import('../../src/legacy/SlideMenu/export'),
+  'slide-menu': () => import('../../src/legacy/SlideMenu/export'),
+  'slide-menu-next': () => import('../../src/components/SlideMenu/export'),
 };
