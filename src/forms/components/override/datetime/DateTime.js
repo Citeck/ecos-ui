73ceaae1a7392729f1ba0ea4a9a94c35100c1252
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2019-10-30 18:20:46 +0300

    ECOSCOM-2831 - Values on the form are reset during final validation

--- src/forms/components/override/datetime/DateTime.js
+++ src/forms/components/override/datetime/DateTime.js
@@ -29,13 +29,14 @@ export default class DateTimeComponent extends FormIODateTimeComponent {
   }
 
   // Cause: https://citeck.atlassian.net/browse/ECOSCOM-2673
-  updateValue(flags, value) {
-    const isChanged = super.updateValue(flags, value);
-
-    if (isChanged) {
-      this.setPristine(false);
-    }
-
-    return isChanged;
-  }
+  // пришлось отказаться от этого решения из-за бага https://citeck.atlassian.net/browse/ECOSCOM-2831
+  // updateValue(flags, value) {
+  //   const isChanged = super.updateValue(flags, value);
+  //
+  //   if (isChanged) {
+  //     this.setPristine(false);
+  //   }
+  //
+  //   return isChanged;
+  // }
 }
