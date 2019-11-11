eac4b65b3000f8f30e958e707bf3bc59cdc0c163
atupikova <ann_040294@mail.ru>
2019-11-11 15:32:37 +0300

    ECOSENT-916 removal old menu

--- src/components/SlideMenu/export/sagas.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import slideMenu from '../../../sagas/slideMenu';
-import view from '../../../sagas/view';
-
-export default function* rootSaga(extraArguments) {
-  yield all([slideMenu(extraArguments), view(extraArguments)]);
-}
