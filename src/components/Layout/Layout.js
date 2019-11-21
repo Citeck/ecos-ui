2f790a0c529f7ee654e16ae614c89ba98d47b2c2
Nepritimov <skinet788767@gmail.com>
2019-11-21 19:17:09 +0300

    ECOSENT-1118 - fix layout styles; fix save new layout on dashbord; fix updating dashboard with tabs

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -79,8 +79,12 @@ class Layout extends Component {
   checkWrapperStyle() {
     const { columns } = this.props;
 
-    if (this._wrapperRef.current && Array.isArray(get(columns, '0', null))) {
-      this._wrapperRef.current.style.flexDirection = 'column';
+    if (this._wrapperRef.current) {
+      this._wrapperRef.current.style.flexDirection = '';
+
+      if (Array.isArray(get(columns, '0', null))) {
+        this._wrapperRef.current.style.flexDirection = 'column';
+      }
     }
   }
 
@@ -175,18 +179,18 @@ class Layout extends Component {
     return components;
   }
 
-  renderColumn = (column, index) => {
+  renderColumn = (columns, column, index) => {
     if (Array.isArray(column)) {
       this.checkWrapperStyle();
 
       return (
         <div className="ecos-layout__row" key={index}>
-          {column.map(this.renderColumn)}
+          {column.map(this.renderColumn.bind(this, column))}
         </div>
       );
     }
 
-    const { columns, type, canDragging } = this.props;
+    const { type, canDragging } = this.props;
     const { draggableDestination } = this.state;
     const styles = {
       minWidth: getMinWidthColumn(type, index),
@@ -194,11 +198,12 @@ class Layout extends Component {
       height: '100%',
       borderRadius: '5px'
     };
-    const otherWidth = columns.map(column => column.width || '').filter(item => item !== '');
+    const otherWidth = columns
+      .map(column => column.width || '')
+      .filter(item => item !== '')
+      .reduce((sum, current) => (sum += parseFloat(current)), 0);
     const withoutSize = columns.filter(column => !column.width).length;
-    const availableWidth = otherWidth
-      ? `(100% - ${otherWidth.length > 1 ? `(${otherWidth.join(' + ')})` : otherWidth.join(' + ')})`
-      : '100%';
+    const availableWidth = otherWidth ? `(100% - ${otherWidth})` : '100%';
 
     if (!column.width) {
       styles.width = `calc(${availableWidth} / ${withoutSize})`;
@@ -242,7 +247,7 @@ class Layout extends Component {
 
     return (
       <div className="ecos-layout__wrapper" ref={this._wrapperRef}>
-        {columns && columns.map(this.renderColumn)}
+        {columns && columns.map(this.renderColumn.bind(this, columns))}
       </div>
     );
   }
