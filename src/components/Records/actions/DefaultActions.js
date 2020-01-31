70a8a138dfc16156b9b7bb99977ed5be69426935
Shutov <dmitryshutov1@gmail.com>
2020-01-31 12:57:02 +0300

    ECOSENT-1238 - Ecos UI: Add ability to configure redirect for action ViewAction
    Ecos UI: Add ability to configure redirect for action ViewAction

--- src/components/Records/actions/DefaultActions.js
+++ src/components/Records/actions/DefaultActions.js
@@ -53,7 +53,9 @@ export const ViewAction = {
 
   execute: ({ record, action: { config = {}, context } }) => {
     if (config.viewType === 'task-document-dashboard') {
-      const taskDocumentId = Records.get(record.id).load('wfm:document?id');
+      const taskDocumentId = Records.get(record.id)
+        .load('wfm:document?id')
+        .then(res => res);
       goToCardDetailsPage(taskDocumentId);
       return false;
     }
