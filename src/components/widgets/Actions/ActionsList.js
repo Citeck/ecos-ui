409223838a253fd8cca9a6f7d9623d50db723c54
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2020-01-31 16:57:56 +0300

    ECOSENT-1178 - Ecos UI: Optimization and refactoring
    fix bug

--- src/components/widgets/Actions/ActionsList.js
+++ src/components/widgets/Actions/ActionsList.js
@@ -43,41 +43,42 @@ class ActionsList extends React.Component {
       <div className="ecos-actions-list" ref={forwardedRef}>
         {isLoading && <Loader className="ecos-actions-list__loader" blur />}
         {!isLoading && isEmpty(list) && <InfoText className="ecos-actions-list__text-empty" text={t('records-actions.no-available')} />}
-        {list.map((action, index) => {
-          const hasVariants = !isEmpty(action.variants);
+        {Array.isArray(list) &&
+          list.map((action, index) => {
+            const hasVariants = !isEmpty(action.variants);
 
-          return (
-            <div
-              key={`action-${action.type}-${index}`}
-              className={classNames(
-                'ecos-actions-list__item',
-                { 'ecos-actions-list__item_group': hasVariants },
-                { 'ecos-actions-list__item_disabled': isLoading },
-                { 'ecos-actions-list__item_warning': action.theme }
-              )}
-              onClick={() => (hasVariants ? null : this.onClick(action))}
-            >
-              <div className="ecos-actions-list__item-title">{action.name}</div>
-              {hasVariants && (
-                <div className="ecos-actions-list__item-variants">
-                  {!isEmpty(action.variants) &&
-                    action.variants.map(variant => (
-                      <div
-                        key={`action-${action.type}-${index}-${variant.type}`}
-                        className={classNames('ecos-actions-list__item-variants__item', {
-                          'ecos-actions-list__item-variants__item_disabled': isLoading
-                        })}
-                        onClick={() => this.onClick(action)}
-                      >
-                        {variant.name}
-                      </div>
-                    ))}
-                </div>
-              )}
-              {isMobile && index < list.length - 1 && !hasVariants && <Separator noIndents />}
-            </div>
-          );
-        })}
+            return (
+              <div
+                key={`action-${action.type}-${index}`}
+                className={classNames(
+                  'ecos-actions-list__item',
+                  { 'ecos-actions-list__item_group': hasVariants },
+                  { 'ecos-actions-list__item_disabled': isLoading },
+                  { 'ecos-actions-list__item_warning': action.theme }
+                )}
+                onClick={() => (hasVariants ? null : this.onClick(action))}
+              >
+                <div className="ecos-actions-list__item-title">{action.name}</div>
+                {hasVariants && (
+                  <div className="ecos-actions-list__item-variants">
+                    {!isEmpty(action.variants) &&
+                      action.variants.map(variant => (
+                        <div
+                          key={`action-${action.type}-${index}-${variant.type}`}
+                          className={classNames('ecos-actions-list__item-variants__item', {
+                            'ecos-actions-list__item-variants__item_disabled': isLoading
+                          })}
+                          onClick={() => this.onClick(action)}
+                        >
+                          {variant.name}
+                        </div>
+                      ))}
+                  </div>
+                )}
+                {isMobile && index < list.length - 1 && !hasVariants && <Separator noIndents />}
+              </div>
+            );
+          })}
       </div>
     );
   }
