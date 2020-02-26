6d2c950c46edb7eb93d9e71b8bc225a538a83cd8
Nepritimov <skinet788767@gmail.com>
2020-02-26 14:53:09 +0300

    ECOSENT-1153 - fix timesheet size

--- src/helpers/polyfills.js
+++ src/helpers/polyfills.js
@@ -218,4 +218,19 @@ export const polyfills = () => {
       })(this);
     };
   }
+
+  if (!Object.values) {
+    const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+    const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+    const concat = Function.bind.call(Function.call, Array.prototype.concat);
+    const keys = function(target) {
+      return Object.getOwnPropertyNames(target).concat(Object.getOwnPropertySymbols(target));
+    };
+
+    Object.values = function values(O) {
+      return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+    };
+  }
 };
+
+polyfills();
