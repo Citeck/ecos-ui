721daef7f56e4b2f8a72a6e374ec29d70bb9867d
Maxim Nepritimov <mnepritimov@mmtr.ru>
2020-03-06 16:03:25 +0300

    ECOSCOM-2977 - fix form editor style

--- src/forms/components/custom/selectAction/editForm/SelectAction.edit.data.js
+++ src/forms/components/custom/selectAction/editForm/SelectAction.edit.data.js
@@ -15,97 +15,132 @@ export default [
     key: 'source.items',
     components: [
       {
-        label: 'Action name',
-        allowMultipleMasks: false,
-        showWordCount: false,
-        showCharCount: false,
-        tableView: true,
-        alwaysEnabled: false,
-        type: 'textfield',
-        input: true,
-        key: 'name',
-        placeholder: 'Enter action name',
-        widget: {
-          type: ''
-        },
-        row: '0-0'
-      },
-      {
-        label: 'Action type',
-        type: 'select',
-        input: false,
-        key: 'type',
-        dataSrc: 'values',
-        data: {
-          values: [Types.JS, Types.TRIGGER]
-        },
-        row: '0-1'
-      },
-      {
-        label: 'Action',
-        type: 'panel',
-        customClass: 'formio-select-action__editor-group',
-        components: [
+        label: '',
+        key: 'action',
+        type: 'columns',
+        columns: [
           {
-            type: 'htmlelement',
-            tag: 'div',
-            customClass: 'mt-2',
-            content: '<p>Select action type</p>',
-            conditional: {
-              json: {
-                and: [{ '==': [{ var: 'row.type' }, false] }]
-              }
-            }
-          },
-          {
-            type: 'panel',
-            title: 'JavaScript',
-            collapsible: true,
-            collapsed: true,
-            key: 'code-panel',
             components: [
               {
-                type: 'textarea',
-                key: 'code',
-                rows: 5,
-                editor: 'ace',
-                hideLabel: true,
-                input: true
-              },
-              {
-                type: 'htmlelement',
-                tag: 'div',
-                content: '<p>Enter custom javascript code.</p>'
+                label: 'Action name',
+                allowMultipleMasks: false,
+                showWordCount: false,
+                showCharCount: false,
+                tableView: true,
+                alwaysEnabled: false,
+                type: 'textfield',
+                input: true,
+                key: 'name',
+                placeholder: 'Enter action name',
+                widget: {
+                  type: ''
+                }
               }
             ],
-            conditional: {
-              json: {
-                and: [{ '==': [{ var: 'row.type' }, Types.JS.value] }]
-              }
-            }
+            xs: 0,
+            sm: 12,
+            md: 6,
+            lg: 0,
+            xl: 0,
+            classes: ''
           },
           {
-            label: false,
-            allowMultipleMasks: false,
-            showWordCount: false,
-            showCharCount: false,
-            tableView: true,
-            alwaysEnabled: false,
-            type: 'textfield',
-            input: true,
-            key: 'eventName',
-            placeholder: 'Enter event name',
-            widget: {
-              type: ''
-            },
-            conditional: {
-              json: {
-                and: [{ '==': [{ var: 'row.type' }, Types.TRIGGER.value] }]
+            components: [
+              {
+                label: 'Action type',
+                type: 'select',
+                input: false,
+                placeholder: 'Select action type',
+                key: 'type',
+                dataSrc: 'values',
+                data: {
+                  values: [Types.JS, Types.TRIGGER]
+                }
               }
-            }
+            ],
+            xs: 0,
+            sm: 12,
+            md: 6,
+            lg: 0,
+            xl: 0,
+            classes: ''
+          },
+          {
+            components: [
+              {
+                label: 'Action',
+                type: 'panel',
+                customClass: 'formio-select-action__editor-group',
+                components: [
+                  {
+                    type: 'htmlelement',
+                    tag: 'div',
+                    customClass: 'mt-2',
+                    content: '<p>Select action type</p>',
+                    conditional: {
+                      json: {
+                        and: [{ '==': [{ var: 'row.type' }, false] }]
+                      }
+                    }
+                  },
+                  {
+                    type: 'panel',
+                    title: 'JavaScript',
+                    collapsible: true,
+                    collapsed: true,
+                    key: 'code-panel',
+                    components: [
+                      {
+                        type: 'textarea',
+                        key: 'code',
+                        rows: 5,
+                        editor: 'ace',
+                        hideLabel: true,
+                        input: true
+                      },
+                      {
+                        type: 'htmlelement',
+                        tag: 'div',
+                        content: '<p>Enter custom javascript code.</p>'
+                      }
+                    ],
+                    conditional: {
+                      json: {
+                        and: [{ '==': [{ var: 'row.type' }, Types.JS.value] }]
+                      }
+                    }
+                  },
+                  {
+                    label: false,
+                    allowMultipleMasks: false,
+                    showWordCount: false,
+                    showCharCount: false,
+                    tableView: true,
+                    alwaysEnabled: false,
+                    type: 'textfield',
+                    input: true,
+                    key: 'eventName',
+                    placeholder: 'Enter event name',
+                    widget: {
+                      type: ''
+                    },
+                    conditional: {
+                      json: {
+                        and: [{ '==': [{ var: 'row.type' }, Types.TRIGGER.value] }]
+                      }
+                    }
+                  }
+                ]
+              }
+            ],
+            xs: 12,
+            sm: 12,
+            md: 12,
+            lg: 12,
+            xl: 12,
+            classes: ''
           }
-        ],
-        row: '0-2'
+        ]
       }
     ],
     weight: 21
