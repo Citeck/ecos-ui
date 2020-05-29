e48fcaddac8e8f477cc552f87712e43f8a9cd5bf
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2020-05-29 17:32:19 +0300

    ECOSUI-88 - Attribute of Text editor type is not editable from view form
    fix text editors libraries import, use requirejs

--- src/forms/components/override/textarea/TextArea.js
+++ src/forms/components/override/textarea/TextArea.js
@@ -1,8 +1,8 @@
+import _ from 'lodash';
+import NativePromise from 'native-promise-only';
 import Formio from 'formiojs/Formio';
 import FormIOTextAreaComponent from 'formiojs/components/textarea/TextArea';
 import { overrideTriggerChange } from '../misc';
-
-const ACEJS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.1/ace.js';
 
 export default class TextAreaComponent extends FormIOTextAreaComponent {
   constructor(...args) {
@@ -30,29 +30,159 @@ export default class TextAreaComponent extends FormIOTextAreaComponent {
   }
 
   addCKE(element, settings, onChange) {
-    /*
-     * Cause: https://citeck.atlassian.net/browse/ECOSCOM-2477
-     *
-     * В Base.js в методе addCKE происходит подключение библиотеки ckeditor через функцию Formio.requireLibrary
-     * По какой-то причине объект ClassicEditor из библиотеки ckeditor не попадает в глобальную область видимости,
-     * в функции requireLibrary запускается бесконечный setInterval, ожидающий появление ClassicEditor в window.
-     *
-     * Запуск Formio.requireLibrary('ace', 'ace', ACEJS_URL, true); перед super.addCKE
-     * магическим образом решает эту проблему. Пока оставляю этот хак.
-     *
-     * TODO: найти причину такого поведения
-     */
-    Formio.requireLibrary('ace', 'ace', ACEJS_URL, true);
-    return super.addCKE(element, settings, onChange);
+    settings = _.isEmpty(settings) ? {} : settings;
+    settings.base64Upload = true;
+
+    return new Promise(resolve => {
+      window.requirejs(['/js/lib/ckeditor5-build-classic/v12.2.0-formio.2/ckeditor.js'], ckeditor => {
+        if (!element.parentNode) {
+          return NativePromise.reject();
+        }
+        return ckeditor.create(element, settings).then(editor => {
+          editor.model.document.on('change', () => onChange(editor.data.get()));
+          resolve(editor);
+          return editor;
+        });
+      });
+    });
   }
 
   addQuill(element, settings, onChange) {
-    /*
-     * Cause: https://citeck.atlassian.net/browse/ECOSCOM-2477
-     * См. this.addCKE
-     * Здесь аналогичная ситуация, только для Quill
-     */
-    Formio.requireLibrary('ace', 'ace', ACEJS_URL, true);
-    return super.addQuill(element, settings, onChange);
+    settings = _.isEmpty(settings) ? this.wysiwygDefault : settings;
+
+    // Lazy load the quill css.
+    Formio.requireLibrary(
+      `quill-css-${settings.theme}`,
+      'Quill',
+      [{ type: 'styles', src: `https://cdn.quilljs.com/1.3.6/quill.${settings.theme}.css` }],
+      true
+    );
+
+    return new Promise(resolve => {
+      window.requirejs(['/js/lib/quill/1.3.6/quill.js'], Quill => {
+        if (!element.parentNode) {
+          return NativePromise.reject();
+        }
+        this.quill = new Quill(element, settings);
+
+        /** This block of code adds the [source] capabilities.  See https://codepen.io/anon/pen/ZyEjrQ **/
+        const txtArea = document.createElement('textarea');
+        txtArea.setAttribute('class', 'quill-source-code');
+        this.quill.addContainer('ql-custom').appendChild(txtArea);
+        const qlSource = element.parentNode.querySelector('.ql-source');
+        if (qlSource) {
+          this.addEventListener(qlSource, 'click', event => {
+            event.preventDefault();
+            if (txtArea.style.display === 'inherit') {
+              this.quill.setContents(this.quill.clipboard.convert(txtArea.value));
+            }
+            txtArea.style.display = txtArea.style.display === 'none' ? 'inherit' : 'none';
+          });
+        }
+        /** END CODEBLOCK **/
+
+        // Make sure to select cursor when they click on the element.
+        this.addEventListener(element, 'click', () => this.quill.focus());
+
+        // Allows users to skip toolbar items when tabbing though form
+        const elm = document.querySelectorAll('.ql-formats > button');
+        for (let i = 0; i < elm.length; i++) {
+          elm[i].setAttribute('tabindex', '-1');
+        }
+
+        this.quill.on('text-change', () => {
+          txtArea.value = this.quill.root.innerHTML;
+          onChange(txtArea);
+        });
+
+        resolve(this.quill);
+        return this.quill;
+      });
+    });
+  }
+
+  addAce(element) {
+    window.requirejs(['/js/lib/ace/1.4.1/ace.js'], () => {
+      const mode = this.component.as || 'javascript';
+      this.editor = window.ace.edit(element);
+
+      this.editor.on('change', () => this.updateEditorValue(this.editor.getValue()));
+      this.editor.getSession().setTabSize(2);
+      this.editor.getSession().setMode(`ace/mode/${mode}`);
+      this.editor.on('input', () => this.acePlaceholder());
+      setTimeout(() => this.acePlaceholder(), 100);
+      this.editorReadyResolve(this.editor);
+      return this.editor;
+    });
+  }
+
+  enableWysiwyg() {
+    if (this.isPlain || this.options.readOnly || this.options.htmlView) {
+      if (this.autoExpand) {
+        this.element.childNodes.forEach(element => {
+          if (element.nodeName === 'TEXTAREA') {
+            this.addAutoExpanding(element);
+          }
+        });
+      }
+
+      return;
+    }
+
+    if (this.component.editor === 'ace') {
+      this.addAce(this.input);
+      return this.input;
+    }
+
+    if (this.component.editor === 'ckeditor') {
+      const settings = this.component.wysiwyg || {};
+      settings.rows = this.component.rows;
+      this.addCKE(this.input, settings, newValue => this.updateEditorValue(newValue)).then(editor => {
+        this.editor = editor;
+        if (this.options.readOnly || this.component.disabled) {
+          this.editor.isReadOnly = true;
+        }
+        this.editorReadyResolve(this.editor);
+        return editor;
+      });
+      return this.input;
+    }
+
+    // Normalize the configurations.
+    if (
+      this.component.wysiwyg &&
+      (this.component.wysiwyg.hasOwnProperty('toolbarGroups') || this.component.wysiwyg.hasOwnProperty('toolbar'))
+    ) {
+      console.warn(
+        'The WYSIWYG settings are configured for CKEditor. For this renderer, you will need to use configurations for the Quill Editor. See https://quilljs.com/docs/configuration for more information.'
+      );
+      this.component.wysiwyg = this.wysiwygDefault;
+      this.emit('componentEdit', this);
+    }
+    if (!this.component.wysiwyg || typeof this.component.wysiwyg === 'boolean') {
+      this.component.wysiwyg = this.wysiwygDefault;
+      this.emit('componentEdit', this);
+    }
+
+    // Add the quill editor.
+    this.addQuill(this.input, this.component.wysiwyg, () => this.updateEditorValue(this.quill.root.innerHTML))
+      .then(quill => {
+        if (this.component.isUploadEnabled) {
+          const _this = this;
+          quill.getModule('toolbar').addHandler('image', function() {
+            //we need initial 'this' because quill calls this method with its own context and we need some inner quill methods exposed in it
+            //we also need current component instance as we use some fields and methods from it as well
+            _this.imageHandler.call(_this, this);
+          });
+        }
+        quill.root.spellcheck = this.component.spellcheck;
+        if (this.options.readOnly || this.component.disabled) {
+          quill.disable();
+        }
+
+        this.editorReadyResolve(quill);
+        return quill;
+      })
+      .catch(err => console.warn(err));
   }
 }
