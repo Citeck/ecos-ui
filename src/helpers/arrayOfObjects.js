f372daba3dd69eef59345219f4834b6b490a65ea
atupikova <ann_040294@mail.ru>
2020-06-23 16:52:11 +0300

    ECOSENT-1477 lib updated; EditTabs: sort fixed + clean up; Tree: sort fixed

--- src/helpers/arrayOfObjects.js
+++ src/helpers/arrayOfObjects.js
@@ -230,3 +230,18 @@ export function treeMoveItem({ fromId, toId, original, key = 'id' }) {
 
   return items;
 }
+
+export function treeSetDndIndex(items) {
+  const _items = deepClone(items);
+
+  const _set = (list, level, parent) => {
+    list.forEach((item, index) => {
+      item.dndIdx = parseInt(`${level}${parent}${index}`, 10);
+      item.items && _set(item.items, level + 1, parseInt(`${parent}${index}`));
+    });
+  };
+
+  _set(_items, 0, 0);
+
+  return _items;
+}
