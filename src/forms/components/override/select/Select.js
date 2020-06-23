1580cfedfd0c48bba434312a6c62e22295ff1101
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2020-06-23 12:43:05 +0300

    ECOSUI-169 - ECOS Select does not clear current value

---
+++ src/forms/components/override/select/Select.js
@@ -0,0 +1,61 @@
+/* eslint-disable array-callback-return */
+
+import FormIOSelectComponent from 'formiojs/components/select/Select';
+import _ from 'lodash';
+
+export default class SelectComponent extends FormIOSelectComponent {
+  addCurrentChoices(values, items, keyValue) {
+    if (!values) {
+      return false;
+    }
+    const notFoundValuesToAdd = [];
+    const added = values.reduce((defaultAdded, value) => {
+      if (!value) {
+        return defaultAdded;
+      }
+      let found = false;
+
+      // Make sure that `items` and `this.selectOptions` points
+      // to the same reference. Because `this.selectOptions` is
+      // internal property and all items are populated by
+      // `this.addOption` method, we assume that items has
+      // 'label' and 'value' properties. This assumption allows
+      // us to read correct value from the item.
+      const isSelectOptions = items === this.selectOptions;
+      if (items && items.length) {
+        _.each(items, choice => {
+          if (choice._id && value._id && choice._id === value._id) {
+            found = true;
+            return false;
+          }
+          const itemValue = keyValue ? choice.value : this.itemValue(choice, isSelectOptions);
+          found |= _.isEqual(itemValue, value);
+          return found ? false : true;
+        });
+      }
+
+      // Add the default option if no item is found.
+      if (!found) {
+        notFoundValuesToAdd.push({
+          value: this.itemValue(value),
+          label: this.itemTemplate(value)
+        });
+        return true;
+      }
+      return found || defaultAdded;
+    }, false);
+
+    // Cause: https://citeck.atlassian.net/browse/ECOSUI-169
+    // if (notFoundValuesToAdd.length) {
+    //   if (this.choices) {
+    //     this.choices.setChoices(notFoundValuesToAdd, 'value', 'label');
+    //   }
+    //   else {
+    //     notFoundValuesToAdd.map(notFoundValue => {
+    //       this.addOption(notFoundValue.value, notFoundValue.label);
+    //     });
+    //   }
+    // }
+    return added;
+  }
+}
