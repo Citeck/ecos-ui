130958c601b9d86820ae5b3d4aa004b7a83275d7
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2020-07-08 12:32:54 +0300

    ECOSUI-133 - Add ability to map constrained ECOS Select elements

--- src/forms/components/custom/ecosSelect/editForm/EcosSelect.edit.data.js
+++ src/forms/components/custom/ecosSelect/editForm/EcosSelect.edit.data.js
@@ -1,19 +1,37 @@
 import SelectEditData from 'formiojs/components/select/editForm/Select.edit.data';
+import EditFormUtils from 'formiojs/components/base/editForm/utils';
 
 SelectEditData.push(
   {
-    type: 'textarea',
-    input: true,
-    key: 'dataPreProcessingCode',
-    label: 'Data pre-processing',
-    editor: 'ace',
-    rows: 5,
+    type: 'panel',
+    title: 'Data pre-processing',
+    collapsible: true,
+    collapsed: true,
+    customClass: 'form-builder__panel-js',
+    key: 'dataPreProcessingCode-panel',
     weight: 10,
-    placeholder: 'Ex: _.sortBy(queryResult, "label")',
-    tooltip: `Data pre-processing after receiving the specified URL.
-Enter custom JavaScript code.
-<strong>queryResult</strong> - list received by specified api; <strong>_</strong> - instance of Lodash.
-`,
+    components: [
+      EditFormUtils.logicVariablesTable('<tr><th>queryResult</th><td>fetch request result</td></tr>'),
+      {
+        type: 'textarea',
+        key: 'dataPreProcessingCode',
+        rows: 5,
+        editor: 'ace',
+        hideLabel: true,
+        input: true
+      },
+      {
+        type: 'htmlelement',
+        tag: 'div',
+        content: `
+          <small>
+            <p>Data pre-processing after receiving the specified URL. Enter custom JavaScript code.</p>
+            <p>You must assign the <strong>values</strong> variable.</p>
+            <h5>Example:</h5>
+            <pre>values = _.sortBy(queryResult, "label");</pre>
+          </small>`
+      }
+    ],
     conditional: {
       json: {
         and: [{ '===': [{ var: 'data.dataSrc' }, 'url'] }]
