fe22f811e16ce92a72fc3c28469d961324cd5252
Pavel Simonov <pavel.simonov@citeck.ru>
2020-08-12 13:21:43 +0700

    ECOSCOM-3795 - Add create variants from custom journals to create menu

--- src/api/menu.js
+++ src/api/menu.js
@@ -93,7 +93,45 @@ export class MenuApi extends CommonApi {
 
   getCreateVariantsForAllSites = () => {
     const url = `${PROXY_URI}api/journals/create-variants/site/ALL`;
-    return this.getJson(url).catch(() => []);
+
+    const allSites = this.getJson(url).catch(() => []);
+    const fromJournals = Records.query(
+      {
+        sourceId: 'uiserv/journal',
+        language: 'site-journals'
+      },
+      {
+        createVariants: 'createVariants[]{id,attributes:attributes?json,formRef:formRef?id,name,recordRef:recordRef?id}',
+        journalList: 'attributes.journalsListId'
+      }
+    )
+      .then(res => res.records || [])
+      .then(records => {
+        return records
+          .map(r => {
+            let listId = r.journalList;
+            if (!listId) {
+              return {};
+            }
+            let siteId = listId.substring('site-'.length, listId.length - '-main'.length);
+            let variants = (r.createVariants || []).map(v => {
+              return {
+                canCreate: true,
+                formId: v.formRef,
+                title: v.name,
+                ...v
+              };
+            });
+            return {
+              createVariants: variants,
+              siteId: siteId
+            };
+          })
+          .filter(r => r.createVariants && r.createVariants.length);
+      })
+      .catch(() => []);
+
+    return Promise.all([allSites, fromJournals]).then(res => res[0].concat(res[1]));
   };
 
   getCustomCreateVariants = () => {
