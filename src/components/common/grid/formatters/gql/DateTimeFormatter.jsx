f3a9e47b88f111d54b071576f5325fa80e92f694
Alexander Uchaikin <alexander.uchaikin@citeck.ru>
2020-08-19 16:59:45 +0700

    ECOSCOM-3881 - ECOS Journals: Need to add formatter for fields with an only time in value Fixed DateTimeFormatter for work with unformatted time

--- src/components/common/grid/formatters/gql/DateTimeFormatter.jsx
+++ src/components/common/grid/formatters/gql/DateTimeFormatter.jsx
@@ -14,10 +14,25 @@ export default class DateTimeFormatter extends DefaultGqlFormatter {
   render() {
     let { cell, params } = this.props;
 
-    const value = this.value(cell);
+    const value = this.getTimeOrValue(this.value(cell), params);
     const format = params.format || FORMAT;
     const date = value ? moment(value).format(format) : '';
 
     return <Fragment>{date}</Fragment>;
+  }
+
+  getTimeOrValue(value, params) {
+    const unformattedTimeMatcher = /^[0-2][0-9][0-5][0-9][0-5][0-9]$/i;
+    const isUnformattedTime = unformattedTimeMatcher.test(value);
+    const { unformatedValueType } = params || {};
+    if (!isUnformattedTime || (!unformatedValueType && unformatedValueType !== 'unformatted-time')) {
+      return value;
+    }
+
+    const timeSplitter = /.{1,2}/g;
+    const timeArray = value.match(timeSplitter); // [ 'hh', 'mm', 'ss']
+    var time = moment();
+    time.set({ hour: timeArray[0], minute: timeArray[1], second: timeArray[2] });
+    return time;
   }
 }
