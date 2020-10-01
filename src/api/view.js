c2c147dac1f6aea206c5747962456fbd65cb057b
Pavel Simonov <pavel.simonov@citeck.ru>
2020-10-01 08:02:25 +0700

    ECOSUI-585 - Wrong logo on the menu at the qa stand

--- src/api/view.js
+++ src/api/view.js
@@ -1,15 +1,8 @@
 import { CommonApi } from './common';
 import Records from '../components/Records';
-import { MICRO_URI } from '../constants/alfresco';
 import { DEFAULT_THEME } from '../constants/theme';
 
 export class ViewApi extends CommonApi {
-  getCurrentThemeName = siteId => {
-    return this.getHtml(`${MICRO_URI}api/themes/current?siteId=${siteId || ''}`)
-      .then(resp => resp)
-      .catch(() => DEFAULT_THEME);
-  };
-
   getActiveThemeId = () => {
     return Records.get('uiserv/config@active-theme')
       .load('value', true)
