2c48bf5d98580efacb1a965be1aafdb660d4ac20
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2020-10-09 17:50:45 +0300

    ECOSUI-608
    remove console.warn

--- src/helpers/handleControl.js
+++ src/helpers/handleControl.js
@@ -30,8 +30,6 @@ const HCT = HandleControlTypes;
 const LOGOUT_URL_DEFAULT = `${URL_SERVICECONTEXT}dologout`;
 
 export const toggleUnavailableStatus = payload => {
-  console.warn({ payload });
-
   if (!payload.isAvailable) {
     ecosFetch(`${URL_PAGECONTEXT}components/deputy/make-available?available=true`).then(() => {
       window.location.reload();
