007bd540b942edde3499d5477a162b018106bed3
atupikova <ann_040294@mail.ru>
2020-10-13 18:26:46 +0300

    ECOSUI-647 converter value for types; connect

--- src/api/journalsApi.js
+++ src/api/journalsApi.js
@@ -7,12 +7,13 @@ import { ActionModes, Attributes, Permissions } from '../constants';
 import { MICRO_URI, PROXY_URI } from '../constants/alfresco';
 import { debounce, isExistValue } from '../helpers/util';
 import * as ls from '../helpers/ls';
-import { COLUMN_DATA_TYPE_ASSOC, PREDICATE_CONTAINS, PREDICATE_OR } from '../components/common/form/SelectJournal/predicates';
+import { COLUMN_DATA_TYPE_ASSOC, PREDICATE_CONTAINS, PREDICATE_OR } from '../components/Records/predicates/predicates';
 import GqlDataSource from '../components/common/grid/dataSource/GqlDataSource';
 import TreeDataSource from '../components/common/grid/dataSource/TreeDataSource';
 import Records from '../components/Records';
 import RecordActions from '../components/Records/actions';
 import * as RecordUtils from '../components/Records/utils/recordUtils';
+import * as AttributeUtils from '../components/Records/utils/attStrUtils';
 
 import { DocPreviewApi } from './docPreview';
 import { RecordService } from './recordService';
@@ -96,7 +97,9 @@ export class JournalsApi extends RecordService {
 
     let query = {
       t: 'and',
-      val: val.filter(item => item && isExistValue(item.t) && isExistValue(item.val) && item.val !== '')
+      val: AttributeUtils.convertAttributeValues(val, columns).filter(
+        item => item && isExistValue(item.t) && isExistValue(item.val) && item.val !== ''
+      )
     };
     let language = 'predicate';
     if (queryData) {
@@ -187,12 +190,11 @@ export class JournalsApi extends RecordService {
     const queryPredicates = journalPredicate ? [journalPredicate] : [];
     let query = {
       t: 'and',
-      val: queryPredicates.concat(
-        ((Array.isArray(predicates) && predicates) || []).filter(item => {
-          return item.val !== '' && item.val !== null;
-        })
-      )
+      val: queryPredicates.concat(Array.isArray(predicates) ? predicates : [])
     };
+
+    query = AttributeUtils.convertAttributeValues(query, columns).filter(item => item.val !== '' && isExistValue(item.val));
+
     let language = 'predicate';
     if (queryData) {
       query = {
