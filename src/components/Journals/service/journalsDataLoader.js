84fff8e1c2e1a30e79805473f28d54cfd13272d6
atupikova <ann_040294@mail.ru>
2020-10-14 12:18:05 +0300

    ECOSUI-647 connected for dev

--- src/components/Journals/service/journalsDataLoader.js
+++ src/components/Journals/service/journalsDataLoader.js
@@ -4,6 +4,7 @@ import { Attributes } from '../../../constants';
 import AttributesService from '../../../services/AttributesService';
 import JournalsConverter from '../../../dto/journals';
 import { COLUMN_DATA_TYPE_ASSOC, PREDICATE_AND, PREDICATE_CONTAINS, PREDICATE_OR } from '../../Records/predicates/predicates';
+import * as AttributeUtils from '../../Records/utils/attStrUtils';
 import * as RecordUtils from '../../Records/utils/recordUtils';
 import journalsServiceApi from './journalsServiceApi';
 
@@ -29,6 +30,8 @@ class JournalsDataLoader {
 
     let language = 'predicate';
     let query = JournalsConverter.optimizePredicate({ t: PREDICATE_AND, val: predicates });
+
+    query = AttributeUtils.convertAttributeValues(query, columns);
 
     let queryData = null;
     if (journalConfig.queryData || settings.queryData) {
