36de08305c8775a044a1288eeecf643cdba426a6
atupikova <ann_040294@mail.ru>
2020-10-22 14:44:03 +0300

    ECOSUI-665 corrected back for modal form

--- src/components/common/dialogs/Manager/DialogManager.js
+++ src/components/common/dialogs/Manager/DialogManager.js
@@ -319,7 +319,7 @@ const dialogsById = {
         isOpen={isVisible}
         hideModal={hideModal}
         className={classnames('ecos-dialog ecos-dialog_form', modalClass)}
-        reactstrapProps={{ backdrop: false, ...reactstrapProps }}
+        reactstrapProps={reactstrapProps}
       >
         <div className="ecos-dialog__body">
           <FormWrapper isVisible {...formProps} />
