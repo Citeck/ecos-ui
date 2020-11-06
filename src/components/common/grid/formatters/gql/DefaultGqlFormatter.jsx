3721e3498b5e1953d6fe38180c6795a2272982e3
Pavel Simonov <pavel.simonov@citeck.ru>
2020-11-06 09:37:18 +0700

    ECOSCOM-4017 - Develop alfresco model generator based on ECOS types

--- src/components/common/grid/formatters/gql/DefaultGqlFormatter.jsx
+++ src/components/common/grid/formatters/gql/DefaultGqlFormatter.jsx
@@ -2,6 +2,12 @@ import BaseFormatter from '../BaseFormatter';
 
 export default class DefaultGqlFormatter extends BaseFormatter {
   static getQueryString(attribute) {
+    if (!attribute) {
+      return attribute;
+    }
+    if (attribute[0] === '?' || attribute[0] === '.') {
+      return attribute;
+    }
     return `${attribute}?disp`;
   }
 }
