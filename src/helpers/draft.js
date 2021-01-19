910a467183fbd7a3aaca5b0b7761dc108b257f0f
Maxim Nepritimov <mnepritimov@mmtr.ru>
2021-01-19 13:06:02 +0000

    Merged in feature/ECOSUI-801 (pull request #908)
    Feature/ECOSUI-801
    
    * ECOSUI-810 - open link editor with data about link: text, url
    
    * ECOSUI-810 - remove all link by cursor position
    
    * ECOSUI-810 - fixed for edit and delete functional
    
    * ECOSUI-810 - added icon to input; fix remove functional
    
    * ECOSUI-810 - refactoring; fixed edit link; fixed styles
    
    * ECOSUI-801 - fixed add|edit|remove link for many blocks
    
    * ECOSUI-801 - fixed multiline editing
    
    * ECOSUI-801 - fixed selected bugs; added tests; fixed helpers bugs
    
    * ECOSUI-801 - update font; fix small bug; updated tests
    
    * ECOSUI-801 - rm unused code

---
+++ src/helpers/draft.js
@@ -0,0 +1,178 @@
+import { EditorState, SelectionState } from 'draft-js';
+
+export const getNewEditorStateWithAllBlockSelected = editorState => {
+  if (!editorState) {
+    console.warn('editorState is empty!');
+    return null;
+  }
+
+  const selection = editorState.getSelection();
+  const startKey = selection.getStartKey();
+  const startOffset = selection.getStartOffset();
+  const contentState = editorState.getCurrentContent();
+  const block = contentState.getBlockForKey(startKey);
+  const linkKey = block.getEntityAt(startOffset);
+
+  if (linkKey === null) {
+    return editorState;
+  }
+
+  let start = 0;
+  let end = 0;
+
+  block.findEntityRanges(
+    item => item.getEntity() === linkKey,
+    (startOffset, endOffset) => {
+      start = startOffset;
+      end = endOffset;
+    }
+  );
+
+  if (start === end) {
+    return editorState;
+  }
+
+  const newSelection = SelectionState.createEmpty(startKey)
+    .set('anchorOffset', start)
+    .set('focusOffset', end);
+
+  return EditorState.acceptSelection(editorState, newSelection);
+};
+
+export const getSelectionText = editorState => {
+  if (!editorState) {
+    console.warn('editorState is empty!');
+    return '';
+  }
+
+  const selection = editorState.getSelection();
+  const startKey = selection.getStartKey();
+  const startOffset = selection.getStartOffset();
+  const endOffset = selection.getEndOffset();
+  const contentState = editorState.getCurrentContent();
+  const block = contentState.getBlockForKey(startKey);
+
+  return block
+    .getText()
+    .slice(startOffset, endOffset)
+    .trim();
+};
+
+export const getSelectedBlocks = (editorState, startKey, endKey) => {
+  if (!editorState) {
+    console.warn('editorState is empty!');
+    return null;
+  }
+
+  const currentSelection = editorState.getSelection();
+  const contentState = editorState.getCurrentContent();
+
+  if (startKey === undefined) {
+    startKey = currentSelection.getStartKey();
+  }
+
+  if (endKey === undefined) {
+    endKey = currentSelection.getEndKey();
+  }
+
+  const isSameBlock = startKey === endKey;
+  const startingBlock = contentState.getBlockForKey(startKey);
+  const selectedBlocks = [startingBlock];
+
+  if (!isSameBlock) {
+    let blockKey = startKey;
+
+    while (blockKey !== endKey) {
+      const nextBlock = contentState.getBlockAfter(blockKey);
+
+      selectedBlocks.push(nextBlock);
+      blockKey = nextBlock.getKey();
+    }
+  }
+
+  return selectedBlocks;
+};
+
+export const modifierSelectedBlocks = (editorState, modifier, ...args) => {
+  if (!editorState) {
+    console.warn('editorState is empty!');
+    return null;
+  }
+
+  const currentSelection = editorState.getSelection();
+  const startKey = currentSelection.getStartKey();
+  const endKey = currentSelection.getEndKey();
+  const startOffset = currentSelection.getStartOffset();
+  const endOffset = currentSelection.getEndOffset();
+  const isSameBlock = startKey === endKey;
+  const selectedBlocks = getSelectedBlocks(editorState, startKey, endKey);
+
+  if (!selectedBlocks) {
+    console.warn('selectedBlocks is empty!');
+    return null;
+  }
+
+  let finalEditorState = editorState;
+
+  selectedBlocks.forEach(block => {
+    const currentBlockKey = block.getKey();
+    let selectionStart;
+    let selectionEnd;
+
+    if (currentBlockKey === startKey) {
+      selectionStart = startOffset;
+      selectionEnd = isSameBlock ? endOffset : block.getText().length;
+    } else if (currentBlockKey === endKey) {
+      selectionStart = isSameBlock ? startOffset : 0;
+      selectionEnd = endOffset;
+    } else {
+      selectionStart = 0;
+      selectionEnd = block.getText().length;
+    }
+
+    const selection = SelectionState.createEmpty(currentBlockKey)
+      .set('anchorOffset', selectionStart)
+      .set('focusOffset', selectionEnd);
+
+    finalEditorState = modifier(finalEditorState, selection, ...args);
+  });
+
+  return EditorState.forceSelection(finalEditorState, currentSelection);
+};
+
+export const getFirstSelectedLink = editorState => {
+  if (!editorState) {
+    console.warn('editorState is empty!');
+    return '';
+  }
+
+  const selection = editorState.getSelection();
+  const contentState = editorState.getCurrentContent();
+  const firstLink = contentState
+    .getBlockForKey(selection.getAnchorKey())
+    .getCharacterList()
+    .slice(selection.getStartOffset(), selection.getEndOffset())
+    .find(v => {
+      const entity = v.getEntity();
+
+      return !!entity && contentState.getEntity(entity).getType() === 'LINK';
+    });
+
+  if (firstLink) {
+    return contentState.getEntity(firstLink.getEntity()).getData().url;
+  }
+
+  return '';
+};
+
+export const BUTTONS_TYPE = {
+  BOLD: 'BOLD',
+  ITALIC: 'ITALIC',
+  UNDERLINE: 'UNDERLINE',
+  LIST: 'unordered-list-item',
+  LINK: 'LINK'
+};
+
+export const KEY_COMMANDS = {
+  SEND: 'comment-send'
+};
