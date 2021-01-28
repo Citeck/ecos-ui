cdb8bf8e9b2ee2112a76b462fe9964f4ce2be908
Pavel Simonov <pavel.simonov@citeck.ru>
2021-01-28 12:45:10 +0700

    Add 'args' parameter for 'open-url' action

--- src/components/Records/actions/handler/executor/OpenUrlAction.js
+++ src/components/Records/actions/handler/executor/OpenUrlAction.js
@@ -1,5 +1,6 @@
 import ActionsExecutor from '../ActionsExecutor';
 
+import * as queryString from 'query-string';
 import PageService from '../../../../../services/PageService';
 
 export default class OpenUrlAction extends ActionsExecutor {
@@ -8,11 +9,15 @@ export default class OpenUrlAction extends ActionsExecutor {
   async execForRecord(record, action, context) {
     const { config = {} } = action;
     //todo: this is not required. RecordActions service should replace it before call execForRecord. Check and remove it
-    const url = config.url.replace('${recordRef}', record.id); // eslint-disable-line no-template-curly-in-string
+    let url = config.url.replace('${recordRef}', record.id); // eslint-disable-line no-template-curly-in-string
 
     if (!url) {
       console.error(action);
       throw new Error('URL is a mandatory parameter! Record: ' + record.id + ' Action: ' + action.id);
+    }
+
+    if (config.args) {
+      url = `${url}?${queryString.stringify({ ...config.args })}`;
     }
 
     if (config.withinEcosTab === true) {
