5ca2bf32209e0d3eba2e9795ef45efad270f5621
Ivan Tkachenko <ivan.tkachenko@citeck.ru>
2021-02-01 16:23:44 +0300

    ECOSUI-359 - Connect local UI proxy to server with KeyCloak
    Update token when expired

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,7 +5,7 @@ import { setCookie } from '../helpers/util';
 
 const EIS_CONFIG_URL = '/eis.json';
 const KC_TOKEN_COOKIE_NAME = 'PA';
-const KEYCLOAK_DEFAULT_CLIENT_ID = 'web';
+const KC_DEFAULT_CLIENT_ID = 'web';
 
 class AuthService {
   constructor() {
@@ -30,10 +30,26 @@ class AuthService {
       const config = {
         url: `https://${eisConfig.eisId}/auth`,
         realm: eisConfig.realmId,
-        clientId: process.env.REACT_APP_KEYCLOAK_CONFIG_CLIENT_ID || KEYCLOAK_DEFAULT_CLIENT_ID
+        clientId: process.env.REACT_APP_KEYCLOAK_CONFIG_CLIENT_ID || KC_DEFAULT_CLIENT_ID
       };
 
       this._kc = Keycloak(config);
+
+      this._kc.onTokenExpired = () => {
+        this._kc
+          .updateToken()
+          .then(isUpdated => {
+            if (!isUpdated) {
+              console.error('[kc] Failure to update token');
+              return this._kc.login();
+            }
+            this._saveToken(this._kc.token);
+          })
+          .catch(e => {
+            console.error('[kc] Failure to update token', e);
+            this._kc.login();
+          });
+      };
 
       return this._kc
         .init({
@@ -47,7 +63,7 @@ class AuthService {
           this._saveToken(this._kc.token);
         })
         .catch(e => {
-          console.log('[kc] Init error', e);
+          console.error('[kc] Init error', e);
         });
     });
   };
