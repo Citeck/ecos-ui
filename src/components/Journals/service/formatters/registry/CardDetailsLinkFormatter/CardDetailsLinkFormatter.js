cbdc44cb2c8b31f1fd4df904013604ab62d8fdf1
Nepritimov Maxim <mnepritimov@mmtr.ru>
2021-02-25 18:22:47 +0300

    ECOSUI-977 - rm unused code

--- src/components/Journals/service/formatters/registry/CardDetailsLinkFormatter/CardDetailsLinkFormatter.js
+++ src/components/Journals/service/formatters/registry/CardDetailsLinkFormatter/CardDetailsLinkFormatter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import BaseFormatter from '../BaseFormatter';
-import { createDocumentUrl, isNewVersionPage } from '../../../../../../helpers/urls';
+import { createDocumentUrl } from '../../../../../../helpers/urls';
 import { REMOTE_TITLE_ATTR_NAME } from '../../../../../../constants/pageTabs';
 
 export default class CardDetailsLinkFormatter extends BaseFormatter {
@@ -10,18 +10,9 @@ export default class CardDetailsLinkFormatter extends BaseFormatter {
   format(props) {
     const { row = {}, cell } = props;
     const url = createDocumentUrl(row.id);
-    let linkProps = {};
-
-    if (isNewVersionPage()) {
-      linkProps = {
-        target: '_blank',
-        rel: 'noopener noreferrer',
-        ...{ [REMOTE_TITLE_ATTR_NAME]: true }
-      };
-    }
 
     return (
-      <a href={url} {...linkProps}>
+      <a href={url} target="_blank" rel="noopener noreferrer" {...{ [REMOTE_TITLE_ATTR_NAME]: true }}>
         {cell}
       </a>
     );
