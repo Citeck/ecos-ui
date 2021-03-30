1f75c0967f7aebdf3c5c948ef21ab5532253761b
atupikova <ann_040294@mail.ru>
2021-03-30 16:05:31 +0300

    ECOSUI-1049 - Add pre processing to ui actions tests

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1041,5 +1041,18 @@ export function getColorByString(string = '') {
   return `#${hexNumber}`;
 }
 
+export function getModule(srcModule) {
+  return new Promise((resolve, reject) => {
+    window.require(
+      [srcModule],
+      module => resolve(module),
+      error => {
+        console.error(error);
+        reject(error);
+      }
+    );
+  });
+}
+
 lodashSet(window, 'Citeck.helpers.getCurrentLocale', getCurrentLocale);
 lodashSet(window, 'Citeck.helpers.getMLValue', getMLValue);
