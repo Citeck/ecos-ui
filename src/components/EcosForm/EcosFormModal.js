c8b4c1964903a281242d8d1f43ca083214b80ef3
Nepritimov Maxim <mnepritimov@mmtr.ru>
2021-04-09 10:56:51 +0300

    ECOSUI-1096 - check form modal state

--- src/components/EcosForm/EcosFormModal.js
+++ src/components/EcosForm/EcosFormModal.js
@@ -28,14 +28,17 @@ export default class EcosFormModal extends React.Component {
 
     this.state = {
       isModalOpen: false,
-      isConfigurableForm: false
+      isConfigurableForm: false,
+      addedListener: false
     };
   }
 
   componentWillUpdate(nextProps, nextState) {
     if (nextProps.isModalOpen !== this.state.isModalOpen) {
-      if (nextProps.isModalOpen) {
+      if (nextProps.isModalOpen && !nextState.addedListener) {
         window.addEventListener('beforeunload', this._onbeforeunload);
+
+        this.setState({ addedListener: true });
       }
 
       this.setState({ isModalOpen: nextProps.isModalOpen });
@@ -97,10 +100,7 @@ export default class EcosFormModal extends React.Component {
       onCancelModal();
     }
 
-    this.setState({
-      isModalOpen: false,
-      recordData: null
-    });
+    this.setState({ isModalOpen: false });
   };
 
   hide() {
@@ -132,6 +132,10 @@ export default class EcosFormModal extends React.Component {
   }
 
   _onbeforeunload = e => {
+    if (!this.state.isModalOpen) {
+      return;
+    }
+
     // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#Examples
     e.preventDefault();
     e.returnValue = '';
