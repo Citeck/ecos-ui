38d28e5e1ab639a4ffdac029d8325d076a130e51
atupikova <G6u8sMwq7z>
2021-04-19 11:52:07 +0300

    ECOSUI-915 - Put categories in container for scrolling

--- src/constants/bpmn.js
+++ src/constants/bpmn.js
@@ -22,12 +22,9 @@ export const SORT_FILTER_OLD = 1;
 export const SORT_FILTER_AZ = 2;
 export const SORT_FILTER_ZA = 3;
 
-export const ROOT_CATEGORY_NODE_REF = 'workspace://SpacesStore/ecos-bpm-category-root';
+export const ROOT_CATEGORY_NODE_REF = 'alfresco/@workspace://SpacesStore/ecos-bpm-category-root';
 
 export const EDITOR_PAGE_CONTEXT = '/share/page/bpmn-editor/';
-export const DESIGNER_PAGE_CONTEXT = '/v2/bpmn-designer';
-
-export const PROCESS_MODEL_NODE_TYPE = 'ecosbpm:processModel';
 
 export const LOCAL_STORAGE_KEY_PAGE_POSITION = 'BpmnPagePosition';
 export const LOCAL_STORAGE_KEY_REFERER_PAGE_PATHNAME = 'BpmnRefererPagePathName';
