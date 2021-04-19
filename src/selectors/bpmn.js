4d29692ff581e21bb844fb754b6af4f620ab4a10
atupikova <G6u8sMwq7z>
2021-04-19 12:59:20 +0300

    ECOSUI-915 - Put categories in container for scrolling

--- src/selectors/bpmn.js
+++ src/selectors/bpmn.js
@@ -1,4 +1,6 @@
 import { createSelector } from 'reselect';
+import endsWith from 'lodash/endsWith';
+
 import { isCategoryHasChildren, compareLastModified, compareOld, compareAZ, compareZA } from '../helpers/bpmn';
 import { SORT_FILTER_LAST_MODIFIED, SORT_FILTER_OLD, SORT_FILTER_AZ, SORT_FILTER_ZA, ROOT_CATEGORY_NODE_REF } from '../constants/bpmn';
 
@@ -55,7 +57,7 @@ export const selectCategoriesByParentId = createSelector(
         return !item.parentId;
       }
 
-      if (!item.parentId.endsWith(parentId)) {
+      if (!endsWith(item.parentId, parentId)) {
         return false;
       }
 
