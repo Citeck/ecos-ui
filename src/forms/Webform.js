aaf99e3a8f2a234c2858815b3429969cda9226e7
Nepritimov Maxim <mnepritimov@mmtr.ru>
2021-04-30 19:00:16 +0300

    ECOSUI-1110 - submit with loader - wip

--- src/forms/Webform.js
+++ src/forms/Webform.js
@@ -1,11 +1,16 @@
 import Webform from 'formiojs/Webform';
 import cloneDeep from 'lodash/cloneDeep';
 import merge from 'lodash/merge';
+
 import { OUTCOME_BUTTONS_PREFIX } from '../constants/forms';
 
 const originalSetElement = Webform.prototype.setElement;
 const originalOnSubmit = Webform.prototype.onSubmit;
 const originalSubmit = Webform.prototype.submit;
+const originalOnSubmissionError = Webform.prototype.onSubmissionError;
+const originalSetAlert = Webform.prototype.showErrors;
+const originalcheckData = Webform.prototype.checkData;
+const originalsubmitUrl = Webform.prototype.submitUrl;
 
 Webform.prototype.setElement = function(element) {
   originalSetElement.call(this, element);
@@ -26,11 +31,40 @@ Webform.prototype.setElement = function(element) {
 };
 
 Webform.prototype.onSubmit = function(submission, saved) {
-  const result = originalOnSubmit.call(this, submission, saved);
+  console.warn('onSubmit');
+  this.loading = true;
+  // const result = originalOnSubmit.call(this, submission, saved);
+  this.submitting = false;
+  this.setPristine(true);
+  this.setValue(submission, {
+    noValidate: true,
+    noCheck: true
+  });
+
+  this.setAlert('success', `<p>${this.t('complete')}</p>`);
+
+  if (!submission.hasOwnProperty('saved')) {
+    console.warn("!submission.hasOwnProperty('saved')");
+    submission.saved = saved;
+  }
+
+  this.emit('submit', submission);
+
+  if (saved) {
+    this.emit('submitDone', submission);
+    this.loading = false;
+  }
 
   this.setAlert(false);
 
-  return result;
+  console.warn({ submission });
+
+  // if (!submission.saved) {
+  //   this.loading = false;
+  // }
+
+  return submission;
+  // return result;
 };
 
 Webform.prototype.submit = function(before, options) {
@@ -78,6 +112,52 @@ Webform.prototype.submit = function(before, options) {
 
     fireSubmit(new Date().getTime() + 5000);
   });
+};
+
+Webform.prototype.executeSubmit = function(options = {}) {
+  this.submitted = true;
+  this.submitting = true;
+
+  return this.submitForm(options)
+    .then(({ submission, saved }) => this.onSubmit(submission, saved))
+    .catch(err => {
+      console.warn('reject => ', { err });
+
+      return Promise.reject(this.onSubmissionError(err));
+    });
+};
+
+Webform.prototype.onSubmissionError = function(error) {
+  console.warn('onSubmissionError');
+  // this.loading = false;
+  return originalOnSubmissionError.call(this, error);
+};
+
+Webform.prototype.checkData = function(data, flags, source) {
+  console.warn('checkData => ', { data, flags, source });
+  // this.loading = false;
+  return originalcheckData.call(this, data, flags, source);
+};
+
+Webform.prototype.submitUrl = function(URL, headers) {
+  console.warn('submitUrl => ', { URL, headers });
+  // this.loading = false;
+  return originalsubmitUrl.call(this, URL, headers);
+};
+
+Webform.prototype.showErrors = function(error, triggerEvent) {
+  const errors = originalSetAlert.call(this, error, triggerEvent);
+
+  console.warn('showErrors => ', { error, errors, triggerEvent });
+
+  if (error) {
+    window.setTimeout(() => {
+      this.loading = false;
+    }, 0);
+  }
+
+  // this.loading = false;
+  return errors;
 };
 
 export default Webform;
