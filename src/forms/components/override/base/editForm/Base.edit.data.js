466bafacb92e243aa9944aa4a3891e11816d24b8
atupikova <G6u8sMwq7z>
2021-05-14 10:34:47 +0300

    ECOSUI-126 - "Refresh On" no multi-select for some components new solution

--- src/forms/components/override/base/editForm/Base.edit.data.js
+++ src/forms/components/override/base/editForm/Base.edit.data.js
@@ -1,18 +1,15 @@
 import BaseEditData from 'formiojs/components/base/editForm/Base.edit.data';
+import set from 'lodash/set';
 
-BaseEditData.push(
-  {
-    type: 'checkbox',
-    input: true,
-    weight: 132,
-    key: 'triggerChangeWhenCalculate',
-    label: `Trigger the "change" event if calculated value changed`,
-    tooltip: 'Use it if you need refresh dependent components when this component CALCULATED value changed'
-  },
-  {
-    key: 'refreshOn',
-    multiple: true
-  }
-);
+BaseEditData.push({
+  type: 'checkbox',
+  input: true,
+  weight: 132,
+  key: 'triggerChangeWhenCalculate',
+  label: `Trigger the "change" event if calculated value changed`,
+  tooltip: 'Use it if you need refresh dependent components when this component CALCULATED value changed'
+});
+
+set(BaseEditData.find(item => item.key === 'refreshOn'), 'multiple', true);
 
 export default BaseEditData;
