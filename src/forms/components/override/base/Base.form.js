ca4a190c685b63e29c9a87eb45a5bac290a97694
Nepritimov Maxim <mnepritimov@mmtr.ru>
2021-05-25 21:15:55 +0300

    ECOSUI-1162 - added basic config

--- src/forms/components/override/base/Base.form.js
+++ src/forms/components/override/base/Base.form.js
@@ -6,17 +6,129 @@ import BaseEditLogic from './editForm/Base.edit.logic';
 
 export const baseEditFormConfig = [
   {
-    key: 'base',
-    label: 'Base',
+    key: 'basic',
+    label: 'Basic',
     weight: 0,
     components: [
       {
-        label: 'Base tab first component',
-        labelPosition: 'left-left',
-        type: 'checkbox',
+        weight: 0,
+        type: 'textfield',
         input: true,
-        key: 'removeIndents',
-        defaultValue: false
+        key: 'label',
+        label: 'Label',
+        placeholder: 'Field Label',
+        tooltip: 'The label for this field that will appear next to it.',
+        validate: {
+          required: true
+        }
+      },
+      {
+        weight: 100,
+        type: 'textfield',
+        input: true,
+        key: 'key',
+        label: 'Property Name',
+        tooltip: 'The name of this field in the API endpoint.',
+        validate: {
+          pattern: '(\\w|\\w[\\w-.]*\\w)',
+          patternMessage:
+            'The property name must only contain alphanumeric characters, underscores, dots and dashes and should not be ended by dash or dot.'
+        }
+      },
+      {
+        weight: 200,
+        type: 'checkbox',
+        label: 'Hide Label',
+        tooltip: 'Hide the label of this component. This allows you to show the label in the form builder, but not when it is rendered.',
+        key: 'hideLabel',
+        input: true
+      },
+      // TODO: Display selected value as a text - есть только у SelectOrgstruct, SelectJournal
+      {
+        weight: 400,
+        type: 'textarea',
+        input: true,
+        key: 'tooltip',
+        label: 'Tooltip',
+        placeholder: 'To add a tooltip to this field, enter text here.',
+        tooltip: 'Adds a tooltip to the side of this field.'
+      },
+      {
+        weight: 500,
+        type: 'checkbox',
+        label: 'Multiple Values',
+        tooltip: 'Allows multiple values to be entered for this field.',
+        key: 'multiple',
+        input: true
+      },
+      {
+        weight: 600,
+        type: 'checkbox',
+        label: 'Disabled',
+        tooltip: 'Disable the form input.',
+        key: 'disabled',
+        input: true
+      },
+      {
+        weight: 700,
+        type: 'checkbox',
+        label: 'Hidden',
+        tooltip: 'A hidden field is still a part of the form, but is hidden from view.',
+        key: 'hidden',
+        input: true
+      },
+      {
+        weight: 800,
+        type: 'checkbox',
+        label: 'Required',
+        tooltip: 'A required field must be filled in before the form can be submitted.',
+        key: 'validate.required',
+        input: true
+      },
+      {
+        type: 'panel',
+        title: 'Simple conditional',
+        key: 'simple-conditional',
+        theme: 'default',
+        weight: 900,
+        components: [
+          {
+            type: 'select',
+            input: true,
+            label: 'This component should Display:',
+            key: 'conditional.show',
+            dataSrc: 'values',
+            data: {
+              values: [{ label: 'True', value: 'true' }, { label: 'False', value: 'false' }]
+            }
+          },
+          {
+            type: 'select',
+            input: true,
+            label: 'When the form component:',
+            key: 'conditional.when',
+            dataSrc: 'custom',
+            valueProperty: 'value',
+            data: {
+              custom: `
+            utils.eachComponent(instance.root.editForm.components, function(component, path) {
+              if (component.key !== data.key) {
+                values.push({
+                  label: component.label || component.key,
+                  value: component.key
+                });
+              }
+            });
+          `
+            }
+          },
+          {
+            type: 'textfield',
+            input: true,
+            label: 'Has the value:',
+            key: 'conditional.eq'
+          }
+        ]
       }
     ]
   },
