f46cda240263cf882345ced9e679b31b1f898b8a
atupikova <G6u8sMwq7z>
2021-07-28 18:24:46 +0300

    ECOSUI-1297 - Journal. Optimize loading of data for different views rename class

--- src/components/AdminSection/AdminMenu/AdminMenu.js
+++ src/components/AdminSection/AdminMenu/AdminMenu.js
@@ -37,7 +37,7 @@ const AdminMenu = ({ isMobile, groupSectionList, children, toggle, open, getGrou
         onClick={() => toggle(true)}
         icon="icon-small-arrow-left"
         className={classNames(
-          'ecos-admin-menu__btn-opener ecos-btn_light-blue ecos-btn_hover_dark-blue ecos-btn_narrow-t_standart ecos-btn_r_biggest',
+          'ecos-admin-menu__btn-opener ecos-btn_light-blue ecos-btn_hover_dark-blue ecos-btn_narrow-t_standard ecos-btn_r_biggest',
           { 'ecos-admin-menu__btn-opener_hidden': open }
         )}
       >
@@ -53,7 +53,7 @@ const AdminMenu = ({ isMobile, groupSectionList, children, toggle, open, getGrou
             onClick={() => toggle(false)}
             icon="icon-small-arrow-right"
             invert
-            className="ecos-admin-menu__btn-closer ecos-btn_grey5 ecos-btn_hover_grey ecos-btn_narrow-t_standart ecos-btn_r_biggest"
+            className="ecos-admin-menu__btn-closer ecos-btn_grey5 ecos-btn_hover_grey ecos-btn_narrow-t_standard ecos-btn_r_biggest"
           >
             {isMobile ? t(Labels.HIDE_MENU_sm) : t(Labels.HIDE_MENU)}
           </IcoBtn>
