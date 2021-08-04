2c437a13237f7c0479d9ebf3dddcc971e51b4b65
Nepritimov Maxim <mnepritimov@mmtr.ru>
2021-08-04 15:03:24 +0300

    ECOSUI-1220 - rm textfield divider

--- src/components/common/form/Input/MLText.js
+++ src/components/common/form/Input/MLText.js
@@ -14,7 +14,6 @@ class MLText extends BaseMLField {
       <div style={style} className={classNames('ecos-ml-text', className)}>
         <Input {...this.inputProps} value={this.value} onChange={this.handleChangeText} />
         {this.renderLang()}
-        <div className="ecos-ml-text__divider" />
       </div>
     );
   }
