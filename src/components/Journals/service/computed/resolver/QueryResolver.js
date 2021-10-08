ee99fccc5abd725edab47b0ac983eca75b2ee088
Pavel Simonov <pavel.simonov@citeck.ru>
2021-10-08 13:38:40 +0700

    ECOSUI-1449 - Add type records computed resolver for journals

--- src/components/Journals/service/computed/resolver/QueryResolver.js
+++ src/components/Journals/service/computed/resolver/QueryResolver.js
@@ -7,10 +7,19 @@ export default class QueryResolver extends ComputedResolver {
   async resolve(config) {
     const attributes = config.attributes;
     let result;
+    const query = {
+      ...config.query
+    };
+    if (query.language == null) {
+      query.language = 'predicate';
+    }
+    if (query.page == null) {
+      query.page = { maxItems: 100 };
+    }
     if (!attributes) {
-      result = journalsServiceApi.queryData(config.query);
+      result = journalsServiceApi.queryData(query);
     } else {
-      result = journalsServiceApi.queryData(config.query, attributes);
+      result = journalsServiceApi.queryData(query, attributes);
     }
     return result
       .then(res => res.records)
