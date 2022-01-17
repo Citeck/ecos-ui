238f04f970c47f87f63acfca49ec78dd2b99b9d1
Nepritimov Maxim <mnepritimov@mmtr.ru>
2022-01-17 20:17:01 +0300

    ECOSUI-1590 - added flag and condition; checking Base components

---
+++ src/forms/components/override/base/editForm/Base.edit.validation.js
@@ -0,0 +1,15 @@
+import BaseEditValidation from 'formiojs/components/base/editForm/Base.edit.validation';
+
+BaseEditValidation.push({
+  type: 'checkbox',
+  input: true,
+  weight: 1,
+  key: 'optionalWhenDisabled',
+  label: 'Optional when disabled',
+  tooltip: 'Allow form saving if the field is both disabled, required and empty',
+  conditional: {
+    json: { var: 'data.validate.required' }
+  }
+});
+
+export default BaseEditValidation;
--- src/forms/components/override/base/index.js
+++ src/forms/components/override/base/index.js
@@ -2,5 +2,6 @@ import BaseComponent from './Base';
 import './editForm/Base.edit.data';
 import './editForm/Base.edit.display';
 import './editForm/Base.edit.logic';
+import './editForm/Base.edit.validation';
 
 export default BaseComponent;
