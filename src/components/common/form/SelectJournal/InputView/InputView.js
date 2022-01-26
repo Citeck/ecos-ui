6dab18996c6d99735a168048c4102e2ec25945f6
Alexander <aleksandr.li@citeck.ru>
2022-01-26 10:52:14 +0300

    ECOSUI-1602 - Forms. SelectJounal. Correct ui empty state.

--- src/components/common/form/SelectJournal/InputView/InputView.js
+++ src/components/common/form/SelectJournal/InputView/InputView.js
@@ -185,6 +185,7 @@ class InputView extends Component {
       isCompact,
       editValue,
       placeholder,
+      viewOnly,
       deleteValue,
       selectedRows,
       hideEditRowButton,
@@ -197,7 +198,11 @@ class InputView extends Component {
     }
 
     if (!selectedRows.length) {
-      return <p className="select-journal__value-not-selected">{placeholder ? placeholder : t('select-journal.placeholder')}</p>;
+      return (
+        <p className={classNames('select-journal__value-not-selected', { 'select-journal__value-not-selected_view-only': viewOnly })}>
+          {placeholder ? placeholder : t('select-journal.placeholder')}
+        </p>
+      );
     }
 
     if (viewMode === 'table') {
