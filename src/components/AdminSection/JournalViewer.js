5775befd51fb099e4aee915f406fd2ec4696ca49
Nepritimov Maxim <mnepritimov@mmtr.ru>
2022-02-18 11:50:37 +0300

    ECOSUI-1667 - Searching field doesn't clean when journal was changed
    clear search text if journalId was changed

--- src/components/AdminSection/JournalViewer.js
+++ src/components/AdminSection/JournalViewer.js
@@ -5,15 +5,16 @@ import classNames from 'classnames';
 import pageTabList from '../../services/pageTabs/PageTabList';
 import { getStateId } from '../../helpers/redux';
 import { getId } from '../../helpers/util';
-import { initState } from '../../actions/journals';
+import { initState, setSearchText } from '../../actions/journals';
 import { ErrorBoundary } from '../ErrorBoundary';
 import { Journals } from '../Journals';
 import { getSearchParams } from '../../helpers/urls';
 
-const JournalViewer = React.memo(({ hidden, isActivePage, initStateJournal, upStateId, stateId, ...props }) => {
+const JournalViewer = React.memo(({ hidden, isActivePage, initStateJournal, upStateId, stateId, setJournalSearch, ...props }) => {
   const tableCont = useRef(null);
   const [initialized, setInitialized] = useState(false);
   const [prefixStateId, setPrefixStateId] = useState('');
+  const [searchParams, setSearchParams] = useState({});
 
   useEffect(() => {
     if (isActivePage && !hidden && !initialized) {
@@ -25,14 +26,27 @@ const JournalViewer = React.memo(({ hidden, isActivePage, initStateJournal, upSt
   }, [isActivePage, hidden, initialized]);
 
   useEffect(() => {
-    const journalId = getSearchParams().journalId;
+    const searchParams = getSearchParams();
+    const { journalId } = searchParams;
     const _journalStateId = journalId ? prefixStateId + '-[' + journalId + ']' : undefined;
+
+    setSearchParams(searchParams);
 
     if (isActivePage && !hidden && initialized && journalId && _journalStateId !== stateId) {
       initStateJournal(_journalStateId);
       upStateId(_journalStateId);
     }
   });
+
+  useEffect(() => {
+    const params = getSearchParams();
+
+    setSearchParams(params);
+
+    if (stateId && params.journalId !== searchParams.journalId) {
+      setJournalSearch({ stateId, text: '' });
+    }
+  }, [window.location.search]);
 
   return (
     <div ref={tableCont} className={classNames('ecos-admin-section__journal', { 'd-none': hidden })}>
@@ -58,7 +72,8 @@ const mapStateToProps = (store, props) => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  initStateJournal: stateId => dispatch(initState(stateId))
+  initStateJournal: stateId => dispatch(initState(stateId)),
+  setJournalSearch: data => dispatch(setSearchText(data))
 });
 
 export default connect(
