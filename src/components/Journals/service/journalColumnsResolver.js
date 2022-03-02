53043094889a121fd73c27906e416e5f14b19409
Nepritimov Maxim <mnepritimov@mmtr.ru>
2022-03-02 16:41:02 +0300

    ECOSUI-1692 - Inline editing in journal doesn't saved after Enter was pressed
    fixed change data by Enter inside grid cell editor

--- src/components/Journals/service/journalColumnsResolver.js
+++ src/components/Journals/service/journalColumnsResolver.js
@@ -139,7 +139,6 @@ class JournalColumnsResolver {
         attribute: column.attribute,
         ref: editorProps.ref,
         onUpdate: editorProps.onUpdate,
-        onKeyDown: editorProps.onKeyDown,
         onBlur: editorProps.onBlur,
         editor: column.newEditor,
         multiple: column.multiple,
