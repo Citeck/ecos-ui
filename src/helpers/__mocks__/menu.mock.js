e67960c0d855b29dbb36c10c27e00abbd10b7865
atupikova <G6u8sMwq7z>
2022-03-28 10:22:04 +0300

    ECOSUI-1649 - Invalid menu link fix tests

--- src/helpers/__mocks__/menu.mock.js
+++ src/helpers/__mocks__/menu.mock.js
@@ -147,7 +147,7 @@ export const makeSiteMenuFromConfig = [
       {
         id: 'GO_ADMIN_PAGE',
         label: 'header.site-menu.admin-page',
-        targetUrl: '/v2/admin',
+        targetUrl: '/v2/dev-tools',
         targetUrlType: 'FULL_PATH'
       }
     ]
@@ -178,7 +178,7 @@ export const makeSiteMenuFromConfig = [
       {
         id: 'GO_ADMIN_PAGE',
         label: 'header.site-menu.admin-page',
-        targetUrl: '/v2/admin',
+        targetUrl: '/v2/dev-tools',
         targetUrlType: 'FULL_PATH'
       }
     ]
@@ -194,7 +194,7 @@ export const makeSiteMenuFromConfig = [
       {
         id: 'GO_ADMIN_PAGE',
         label: 'header.site-menu.admin-page',
-        targetUrl: '/v2/admin',
+        targetUrl: '/v2/dev-tools',
         targetUrlType: 'FULL_PATH'
       }
     ]
