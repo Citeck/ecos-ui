a96ed00d76bbfc321e7433238d1809749abb478c
d.selyanina <daraselanina89009@gmail.com>
2022-04-21 14:44:20 +0300

    ECOSUI-1778 - Bpmn editor action buttons replace to icons

--- src/components/ModelEditorWrapper/ModelEditorWrapper.js
+++ src/components/ModelEditorWrapper/ModelEditorWrapper.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-import { isExistValue, t } from '../../helpers/util';
+import { isExistValue, t, isMobileDevice } from '../../helpers/util';
 import { Icon, InfoText } from '../common';
 import { Caption } from '../common/form';
-import { Btn } from '../common/btns';
+import { Btn, IcoBtn } from '../common/btns';
+import { Tooltip } from '../common';
 import TitlePageLoader from '../common/TitlePageLoader';
 
 import './style.scss';
@@ -55,19 +56,22 @@ class ModelEditorWrapper extends React.Component {
               <div className="ecos-model-editor__designer-buttons">
                 {onCreate && <Btn onClick={onCreate}>{t(Labels.CREATE)}</Btn>}
                 {onApply && (
-                  <Btn className="ecos-btn_blue" onClick={onApply}>
-                    {t(Labels.APPLY)}
-                  </Btn>
+                  <>
+                    <IcoBtn icon="fa fa-save" onClick={onApply} id="fa-fa-save" />
+                    <Tooltip trigger="hover" target="fa-fa-save" text={t(Labels.APPLY)} />
+                  </>
                 )}
                 {onViewXml && (
-                  <Btn className="ecos-btn_blue" onClick={onViewXml}>
-                    {t(Labels.VIEW_XML)}
-                  </Btn>
+                  <div>
+                    <IcoBtn icon="icon-document-view" onClick={onViewXml} id="icon-document-view" />
+                    <Tooltip trigger="hover" target="icon-document-view" text={t(Labels.VIEW_XML)} />
+                  </div>
                 )}
                 {onSaveAndDeploy && (
-                  <Btn className="ecos-btn_green" onClick={onSaveAndDeploy}>
-                    {t(Labels.SAVE_DEPLOY)}
-                  </Btn>
+                  <div>
+                    <IcoBtn icon="fa fa-cloud-upload" onClick={onSaveAndDeploy} className="ecos-btn_green" id="fa-fa-cloud-upload" />
+                    <Tooltip trigger="hover" target="fa-fa-cloud-upload" text={t(Labels.SAVE_DEPLOY)} />
+                  </div>
                 )}
               </div>
             </div>
