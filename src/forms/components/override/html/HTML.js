1bcc86f4485acc6f8f6feee25291ebdfbdec71e9
d.selyanina <daraselanina89009@gmail.com>
2022-05-13 13:09:21 +0300

    ECOSUI-1773 - Form Builder. Localization
    fix problems

--- src/forms/components/override/html/HTML.js
+++ src/forms/components/override/html/HTML.js
@@ -3,7 +3,7 @@ import { t } from '../../../../helpers/export/util';
 
 export default class HtmlComponent extends FormIOHTMLComponent {
   setHTML() {
-    if (!t(`form-constructor.html.${this.component.key}`).includes(this.component.key)) {
+    if (t(`form-constructor.html.${this.component.key}`) !== `form-constructor.html.${this.component.key}`) {
       this.htmlElement.innerHTML = this.interpolate(t(`form-constructor.html.${this.component.key}`));
     } else {
       this.htmlElement.innerHTML = this.interpolate(this.t(this.component.content));
