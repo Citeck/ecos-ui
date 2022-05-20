5b7b17a515dc2de8389dd25f5bd20f06201e2c08
Nepritimov Maxim <mnepritimov@mmtr.ru>
2022-05-20 20:00:47 +0300

    ECOSUI-1756 - Need to add the optional ability to limit the width of the TableFrom content
    added noHorizontalScroll parameter; added tests

--- src/forms/components/custom/tableForm/editForm/TableForm.edit.display.js
+++ src/forms/components/custom/tableForm/editForm/TableForm.edit.display.js
@@ -10,6 +10,15 @@ export default [
   {
     type: 'checkbox',
     input: true,
+    key: 'noHorizontalScroll',
+    label: 'Disable horizontal scroll',
+    tooltip: 'Limiting table content width to component width',
+    weight: 18,
+    defaultValue: false
+  },
+  {
+    type: 'checkbox',
+    input: true,
     key: 'isStaticModalTitle',
     label: 'Hide record name in modal title',
     weight: 18,
