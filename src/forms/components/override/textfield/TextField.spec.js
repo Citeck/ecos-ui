75fb29e3cf2cad6e3920b0195d41cb02f8236346
d.selyanina <daraselanina89009@gmail.com>
2022-06-10 09:00:35 +0300

    ECOSUI-1875 - Form Builder. Localization
    вернула тест

--- src/forms/components/override/textfield/TextField.spec.js
+++ src/forms/components/override/textfield/TextField.spec.js
@@ -3,6 +3,7 @@ import TextFieldComponent from './TextField';
 import EventEmitter from '../../../EventEmitter';
 
 import comp1 from './fixtures/comp1';
+import { t } from '../../../../helpers/export/util';
 
 describe('TextField Component', () => {
   it('Should build a TextField component', done => {
