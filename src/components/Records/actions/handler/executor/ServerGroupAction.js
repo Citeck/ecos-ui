f35946a639457d65e2debd40ca1ed8e8a1ebd347
d.selyanina <daraselanina89009@gmail.com>
2022-06-22 11:51:56 +0300

    ECOSUI-1887 - Group operations. No text when error occurs
    add code

--- src/components/Records/actions/handler/executor/ServerGroupAction.js
+++ src/components/Records/actions/handler/executor/ServerGroupAction.js
@@ -23,6 +23,8 @@ const executeAction = async ({ groupAction, selected = [], resolved, query = nul
   }
 
   if (exAction.error) {
+    console.warn({ exAction, groupAction, selected, excludedRecords, resolved, query });
+
     return {
       error: get(exAction, 'error.message') || '-',
       code: get(exAction, 'error.response.status')
