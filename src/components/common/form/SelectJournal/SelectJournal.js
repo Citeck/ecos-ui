8816868fef0ccdb0da4c5597c6310536b879c15b
Alexey <alexey.shlaikov@citeck.ru>
2022-07-07 13:19:00 +0300

    ECOSUI-1925 SelectJournal. Improvement of the selection control from the journal

--- src/components/common/form/SelectJournal/SelectJournal.js
+++ src/components/common/form/SelectJournal/SelectJournal.js
@@ -10,7 +10,7 @@ import isEmpty from 'lodash/isEmpty';
 import isFunction from 'lodash/isFunction';
 
 import { Attributes, Permissions } from '../../../../constants';
-import { beArray, isMobileDevice, t } from '../../../../helpers/util';
+import { getHtmlIdByUid, beArray, isMobileDevice, t } from '../../../../helpers/util';
 import { getIconUpDown } from '../../../../helpers/icon';
 import JournalsConverter from '../../../../dto/journals';
 import JournalsService from '../../../Journals/service';
@@ -22,7 +22,6 @@ import Records from '../../../Records';
 import { parseAttribute } from '../../../Records/utils/attStrUtils';
 import { DialogManager } from '../../dialogs';
 import { matchCardDetailsLinkFormatterColumn } from '../../grid/mapping/Mapper';
-
 import { DataTypes, DisplayModes, Labels } from './constants';
 import InputView from './InputView';
 import ViewMode from './ViewMode';
@@ -775,10 +774,7 @@ export default class SelectJournal extends Component {
             {`${t(Labels.MSG_WHOLE_SELECTION)}. ${t(Labels.SELECTED_LABEL, { data: gridData.total })}`}
           </div>
         )}
-        <div
-          id={journalId ? `${journalId.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '-')}-container` : null}
-          className="select-journal__grid-container"
-        >
+        <div id={getHtmlIdByUid(journalId, 'container')} className="select-journal__grid-container">
           {!isGridDataReady && <Loader />}
 
           <Grid
