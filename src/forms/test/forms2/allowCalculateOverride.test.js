6bba13a118f22351c539f7c0ab85aa60a47faea7
Pavel Simonov <pavel.simonov@citeck.ru>
2022-08-09 11:05:41 +0700

    ECOSENT-2282 Payment. No sum calculations

--- src/forms/test/forms2/allowCalculateOverride.test.js
+++ src/forms/test/forms2/allowCalculateOverride.test.js
@@ -2,7 +2,7 @@ import { TestForm } from './TestForm';
 import assert from 'power-assert';
 
 describe('Calculated fields test #2', () => {
-  it('Should be initialized. Create mode', async done => {
+  it('Allow calculate override. Create mode', async done => {
     const form = await TestForm.create(
       definition,
       { formMode: 'CREATE' },
@@ -14,13 +14,13 @@ describe('Calculated fields test #2', () => {
     let formData = form.getFormData();
     assert.equal(formData.field0, 7);
     assert.equal(formData.field1, 13);
-    assert.equal(formData.totalWithOverride, 20);
+    assert.equal(formData.totalWithoutOverride, 20);
     assert.equal(formData.totalWithOverride, 20);
 
     await form.setInputValue('field0', 10);
     formData = form.getFormData();
     assert.equal(formData.field0, 10);
-    assert.equal(formData.totalWithOverride, 23);
+    assert.equal(formData.totalWithoutOverride, 23);
     assert.equal(formData.totalWithOverride, 23);
 
     await form.setInputValue('totalWithOverride', 1000);
@@ -32,13 +32,65 @@ describe('Calculated fields test #2', () => {
     assert.equal(formData.totalWithoutOverride, 23);
     assert.equal(formData.totalWithOverride, 1000);
 
+    await form.setInputValue('field0', 2000);
+    formData = form.getFormData();
+    assert.equal(formData.field0, 2000);
+    assert.equal(formData.totalWithoutOverride, 2013);
+    assert.equal(formData.totalWithOverride, 1000);
+
     await form.setInputValue('totalWithOverride', null);
     formData = form.getFormData();
     assert.equal(formData.totalWithOverride, '');
 
     await form.setInputValue('totalWithoutOverride', null);
     formData = form.getFormData();
+    assert.equal(formData.totalWithoutOverride, 2013);
+
+    done();
+  });
+
+  it('Allow calculated override test. Edit mode', async done => {
+    const form0 = await TestForm.create(
+      definition,
+      { formMode: 'EDIT' },
+      {
+        field0: 7,
+        field1: 13,
+        totalWithOverride: 10,
+        totalWithoutOverride: 10
+      }
+    );
+
+    let formData = form0.getFormData();
+    assert.equal(formData.field0, 7);
+    assert.equal(formData.field1, 13);
+    assert.equal(formData.totalWithOverride, 10);
+    assert.equal(formData.totalWithoutOverride, 20);
+
+    await form0.setInputValue('field0', 10);
+    formData = form0.getFormData();
+    assert.equal(formData.field0, 10);
+    assert.equal(formData.totalWithOverride, 10);
     assert.equal(formData.totalWithoutOverride, 23);
+
+    const form1 = await TestForm.create(
+      definition,
+      { formMode: 'EDIT' },
+      {
+        field0: 7,
+        field1: 13,
+        totalWithOverride: 20
+      }
+    );
+
+    formData = form1.getFormData();
+    assert.equal(formData.field0, 7);
+    assert.equal(formData.field1, 13);
+    assert.equal(formData.totalWithOverride, 20);
+
+    await form1.setInputValue('field0', 10);
+    formData = form1.getFormData();
+    assert.equal(formData.totalWithOverride, 23);
 
     done();
   });
