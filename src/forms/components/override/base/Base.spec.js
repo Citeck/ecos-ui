c7aa08774fbf36d6bee03f58fd1ebca2c0a662c7
Alexey <alexey.shlaikov@citeck.ru>
2022-08-25 12:16:23 +0300

    Release 2.6.5

--- src/forms/components/override/base/Base.spec.js
+++ src/forms/components/override/base/Base.spec.js
@@ -62,7 +62,7 @@ describe('Base Component', () => {
           bad: {
             value: '',
             field: 'firstName',
-            error: 'First Name is required'
+            error: 'required'
           },
           good: {
             value: 'te'
@@ -86,7 +86,7 @@ describe('Base Component', () => {
           bad: {
             value: 't',
             field: 'firstName',
-            error: 'First Name must be longer than 1 characters.'
+            error: 'minLength'
           },
           good: {
             value: 'te'
@@ -110,7 +110,7 @@ describe('Base Component', () => {
           bad: {
             value: 'testte',
             field: 'firstName',
-            error: 'First Name must be shorter than 5 characters.'
+            error: 'maxLength'
           },
           good: {
             value: 'te'
