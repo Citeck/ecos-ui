753fe4f67115300478ae808da174d0cfe5a6c807
Alexey <alexey.shlaikov@citeck.ru>
2022-08-29 14:09:46 +0300

    ECOSUI-1461 [Forms] Migrate table to use create variants from target type

--- src/components/EcosForm/EcosFormUtils.js
+++ src/components/EcosForm/EcosFormUtils.js
@@ -451,27 +451,31 @@ export default class EcosFormUtils {
     return sourceId + SOURCE_DIVIDER + formId;
   }
 
-  static getCreateVariants(record, attribute) {
-    const recordInstance = isString(record) ? Records.get(record) : record;
+  static async getCreateVariants(record, attribute) {
+    const typeRef = await Records.get(record).load('typeRef?id');
+
+    const recordInstance = typeRef ? Records.get(`emodel/type@${typeRef}`) : isString(record) ? Records.get(record) : record;
     const variantsPromise = recordInstance.load(`attributeById.${attribute}.config.typeRef._as.ref.createVariants[]?json`);
 
-    return variantsPromise.then(variants => {
-      if (!variants) {
-        return [];
+    return variantsPromise.then(async variants => {
+      if (isEmpty(variants)) {
+        const formVariants = await recordInstance.load('#' + attribute + '?createVariants');
+
+        const hasFormPromises = formVariants.map(v => EcosFormUtils.getForm(v.recordRef, v.formKey, 'formKey'));
+
+        return Promise.all(hasFormPromises).then(hasForms => {
+          return formVariants.filter((v, idx) => {
+            let formKey = hasForms[idx];
+            if (!formKey) {
+              return false;
+            }
+            v.formKey = formKey;
+            return true;
+          });
+        });
       }
 
-      let hasFormPromises = variants.map(v => EcosFormUtils.getForm(v.recordRef, v.formKey, 'formKey'));
-
-      return Promise.all(hasFormPromises).then(hasForms => {
-        return variants.filter((v, idx) => {
-          let formKey = hasForms[idx];
-          if (!formKey) {
-            return false;
-          }
-          v.formKey = formKey;
-          return true;
-        });
-      });
+      return variants;
     });
   }
 
