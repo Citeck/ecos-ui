d1e50a559ee95265c1cff4730b7534b0f9de717c
Dmitry.Urgapov <Dmitry.Urgapov@citeck.ru>
2022-09-01 13:11:16 +0300

    Revert "ECOSUI-1837 - Form Builder. Tab duplication"
    This reverts commit 15b134062c26ccb401f267c33258b067fe752543.

--- src/forms/utils.js
+++ src/forms/utils.js
@@ -151,7 +151,16 @@ const _expandEditForm = component => {
       ...tabsByKey.map(item => ({
         ...item,
         key: `${item.key}-reworked`
-      }))
+      })),
+      {
+        key: 'conditional-reworked',
+        components: [
+          {
+            key: 'customConditionalPanel',
+            collapsed: false
+          }
+        ]
+      }
     ]);
 
     _removeDuplicateComponents(get(result, 'components.0.components', []));
