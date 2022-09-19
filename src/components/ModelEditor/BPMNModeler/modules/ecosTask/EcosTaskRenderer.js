d0b5cd9d866b0eae5b85057f0e796f461ce6aa27
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2022-09-19 16:29:34 +0300

    ECOSUI-2137 Add Status value to Set Status bpmn component

--- src/components/ModelEditor/BPMNModeler/modules/ecosTask/EcosTaskRenderer.js
+++ src/components/ModelEditor/BPMNModeler/modules/ecosTask/EcosTaskRenderer.js
@@ -1,15 +1,21 @@
 import BaseRenderer from 'diagram-js/lib/draw/BaseRenderer';
-import { ECOS_TASK_TYPE_SET_STATUS, ECOS_TASK_BASE_ELEMENT } from '../../../../../constants/bpmn';
-
-import { append as svgAppend, attr as svgAttr, classes as svgClasses, create as svgCreate } from 'tiny-svg';
-
+import { append as svgAppend, classes as svgClasses, create as svgCreate, remove as svgRemove, select as svgSelect } from 'tiny-svg';
 import { is, getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
-
+import TextUtil from 'diagram-js/lib/util/Text';
 import { isNil } from 'min-dash';
-
 import _ from 'lodash';
 
+import Records from '../../../../../components/Records/Records';
+import { ECOS_TASK_TYPE_SET_STATUS, ECOS_TASK_BASE_ELEMENT } from '../../../../../constants/bpmn';
+import { t } from '../../../../../helpers/util';
+
 const HIGH_PRIORITY = 1500;
+
+const LABEL_STYLE = {
+  fontFamily: 'Arial, sans-serif',
+  fontSize: '12px',
+  color: '#444444'
+};
 
 export default class CustomRenderer extends BaseRenderer {
   constructor(eventBus, bpmnRenderer) {
@@ -23,6 +29,9 @@ export default class CustomRenderer extends BaseRenderer {
   }
 
   drawShape(parentNode, element) {
+    const rootProcces = this.getParentProccess(element).$parent;
+    const type = this.getEcosType(rootProcces);
+
     const shape = this.bpmnRenderer.drawShape(parentNode, element);
     const statusImage = svgCreate('image', {
       x: 5,
@@ -35,18 +44,21 @@ export default class CustomRenderer extends BaseRenderer {
 
     const statusName = this.getStatusName(element);
 
-    if (!isNil(statusName)) {
-      const text = svgCreate('text');
+    if (!isNil(statusName) && _.isEmpty(this.getName(element))) {
+      Records.get(type)
+        .load('model.statuses[]{value:id,label:name}', false)
+        .then(statuses => {
+          if (!_.isEmpty(statuses)) {
+            const label = statuses.find(field => field.value === statusName)['label'];
+            const text = `${t('dashboard-settings.widget.doc-status')}: "${label}"`;
 
-      svgAttr(text, {
-        fill: '#077683',
-        transform: 'translate(-10, -10)'
-      });
-
-      svgClasses(text).add('djs-label');
-
-      svgAppend(text, document.createTextNode(statusName));
-      svgAppend(parentNode, text);
+            this.renderLabel(parentNode, text, {
+              align: 'center-middle',
+              box: element,
+              padding: 5
+            });
+          }
+        });
     }
 
     svgAppend(parentNode, statusImage);
@@ -54,8 +66,39 @@ export default class CustomRenderer extends BaseRenderer {
     return shape;
   }
 
+  getParentProccess(element) {
+    return getBusinessObject(element).$parent;
+  }
+
+  getName(element) {
+    return getBusinessObject(element).name;
+  }
+
   getStatusName(element) {
     return getBusinessObject(element).status;
+  }
+
+  getEcosType(element) {
+    if (_.isString(element.$attrs['ecos:ecosType'])) {
+      return element.$attrs['ecos:ecosType'];
+    }
+
+    return this.getEcosType(getBusinessObject(element).$parent);
+  }
+
+  renderLabel(parentGfx, label, options) {
+    const textUtil = new TextUtil({
+      style: LABEL_STYLE
+    });
+
+    const text = textUtil.createText(label || '', options);
+    const textNode = svgSelect(parentGfx, 'text');
+
+    svgRemove(textNode);
+    svgClasses(text).add('djs-label');
+    svgAppend(parentGfx, text);
+
+    return text;
   }
 }
 
