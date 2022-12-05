d2f87a3bd62adc038b912d0e6b0e705055c02ccb
Pavel Simonov <pavel.simonov@citeck.ru>
2022-12-05 12:01:37 +0700

    ECOSCOM-4874 - Session expiration on demo does not have checking user activities

--- src/services/config/configApi.js
+++ src/services/config/configApi.js
@@ -6,7 +6,7 @@ const CONFIG_SOURCE_ID_PREFIX = 'uiserv/cfg@';
 export async function loadConfigs(configsMap) {
   const attributesToLoad = {};
   for (let configKey in configsMap) {
-    const innerValue = configsMap[configKey].replace('value?', '?');
+    const innerValue = configsMap[configKey].substring('value'.length);
     attributesToLoad[configKey] = '$cfg.' + configKey + innerValue;
   }
   return Records.get(META_RECORD).load(attributesToLoad);
