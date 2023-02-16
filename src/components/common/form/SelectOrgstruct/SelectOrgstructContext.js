cabfe1145f120b5169ac8ee33a64663d65b888df
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-02-16 16:35:42 +0300

    ECOSUI-2406 First opening orgstructure from All task widget leads to error with task prefix

--- src/components/common/form/SelectOrgstruct/SelectOrgstructContext.js
+++ src/components/common/form/SelectOrgstruct/SelectOrgstructContext.js
@@ -321,13 +321,12 @@ export const SelectOrgstructProvider = props => {
           if (!livePromise) {
             return;
           }
-
-          setTabItems({
-            ...tabItems,
+          setTabItems(prev => ({
+            ...prev,
             [TabTypes.SELECTED]: [...selectedItems],
-            [TabTypes.LEVELS]: tabItems[TabTypes.LEVELS].map(item => setSelectedItem(item, selectedItems)),
-            [TabTypes.USERS]: tabItems[TabTypes.USERS].map(item => setSelectedItem(item, selectedItems))
-          });
+            [TabTypes.LEVELS]: [...prev[TabTypes.LEVELS]].map(item => setSelectedItem(item, selectedItems)),
+            [TabTypes.USERS]: [...prev[TabTypes.USERS]].map(item => setSelectedItem(item, selectedItems))
+          }));
           setSelectedRows([...selectedItems]);
 
           livePromise = false;
