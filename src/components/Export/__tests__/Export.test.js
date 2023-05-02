83859212fa5759ce62addd414b3892bbec016434
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-05-02 12:23:20 +0300

    ECOSCOM-5087 Journals. Export to Excel and CSV does not work

--- src/components/Export/__tests__/Export.test.js
+++ src/components/Export/__tests__/Export.test.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 
 import Export from '../Export';
+import ConfigService from '../../../services/config/ConfigService';
 
 describe('Export component tests', () => {
   test('should render Export component', () => {
+    const spy = jest.spyOn(ConfigService, 'getValue').mockImplementation(() => Promise.resolve(true));
+
     const component = shallow(<Export />);
-    expect(component).toMatchSnapshot();
+
+    expect(spy).toHaveBeenCalled();
+
+    return Promise.resolve(component).then(() => {
+      component.update();
+
+      expect(component).toMatchSnapshot();
+    });
   });
 
   test('should render Export component with empty className props', () => {
@@ -20,11 +30,33 @@ describe('Export component tests', () => {
     expect(component.hasClass('ecos-journal__settings-bar-export')).toEqual(true);
   });
 
-  test('test equal dropdown source and snapshots data', () => {
+  test('test equal dropdown with alfresco source and snapshots data', async () => {
+    const spy = jest.spyOn(ConfigService, 'getValue').mockImplementation(() => Promise.resolve(true));
+
+    const component = mount(<Export />);
+
+    expect(spy).toHaveBeenCalled();
+
+    const dropdownSource = new Export().dropdownSourceWithAlfresco;
+
+    return Promise.resolve(component).then(() => {
+      component.update();
+
+      const list = component.find('ul').props();
+
+      list.children.forEach((node, id) => {
+        expect(node.props.item.id).toEqual(dropdownSource[id]['id']);
+        expect(node.props.item.title).toEqual(dropdownSource[id]['title']);
+      });
+    });
+  });
+
+  test('test equal dropdown withoutAlfresco source and snapshots data', () => {
     const component = mount(<Export />)
       .find('ul')
       .props();
-    const dropdownSource = new Export().dropdownSource;
+
+    const dropdownSource = new Export().dropdownSourceWithoutAlfresco;
 
     component.children.forEach((node, id) => {
       expect(node.props.item.id).toEqual(dropdownSource[id]['id']);
