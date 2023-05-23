e6b00cb1a6944aaf9a0e71bbe5ab84e8532f53a7
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-05-23 17:15:26 +0300

    ECOSUI-2243 Option execAsForRecords in execForQueryConfig actions config doesn't work

--- src/components/Records/actions/recordActions.js
+++ src/components/Records/actions/recordActions.js
@@ -843,15 +843,15 @@ class RecordActions {
 
     let result;
 
+    if (!isEmpty(confirmed)) {
+      RecordActions._fillDataByMap({ action, data: confirmed, sourcePath: 'confirm.', targetPath: 'config.' });
+    }
+
     /** @type {PreProcessActionResult} */
     const preResult = await RecordActions._preProcessAction({ action, context }, 'execForQuery');
 
     if (preResult.configMerged) {
       action.config = preResult.config;
-    }
-
-    if (!isEmpty(confirmed)) {
-      RecordActions._fillDataByMap({ action, data: confirmed, sourcePath: 'confirm.', targetPath: 'config.' });
     }
 
     if (!!get(action, 'execForQueryConfig.execAsForRecords')) {
