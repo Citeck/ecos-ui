4f7ff1f50b17ea484221bbab1fdec70292a75d47
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-05-29 14:45:50 +0300

    ECOSUI-2307 BPMN camunda. Search functionality at bpmn editor blocks browser search functionality

--- src/components/ModelEditor/BPMNModeler/patches/index.js
+++ src/components/ModelEditor/BPMNModeler/patches/index.js
@@ -3,3 +3,4 @@ import './features/modeling/Modeling';
 import './features/modeling/cmd/UpdatePropertiesHandler';
 import './features/palette/PaletteProvider';
 import './features/popup-menu/ReplaceMenuProvider';
+import './features/keyboard/BpmnKeyboardBindings';
