0892d171b197a522c82fb125950ef30f2da01a8f
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-07-27 18:41:32 +0300

    ECOSUI-2679 BPMN: component type is not selected correctly when configured in a scheme

---
+++ src/components/ModelEditor/BPMNModeler/patches/features/context-pad/ContextPadProvider.js
@@ -0,0 +1,29 @@
+import ContextPadProvider from 'bpmn-js/lib/features/context-pad/ContextPadProvider';
+
+import { EVENT_BASED_GATEWAY } from '../../../../../../constants/bpmn';
+
+const originGetContextPadEntries = ContextPadProvider.prototype.getContextPadEntries;
+
+const disabledActionsMap = {
+  [EVENT_BASED_GATEWAY]: [
+    'append.receive-task', // ReceiveTask
+    'append.message-intermediate-event' // MessageIntermediateCatchEvent
+  ]
+};
+
+ContextPadProvider.prototype.getContextPadEntries = function(element) {
+  const actions = originGetContextPadEntries.call(this, element);
+
+  const disabledActions = disabledActionsMap[element.type];
+
+  if (disabledActions && disabledActions.length) {
+    return Object.entries(actions).reduce(
+      (result, [key, value]) => (disabledActions.includes(key) ? result : { ...result, [key]: value }),
+      {}
+    );
+  }
+
+  return actions;
+};
+
+export default ContextPadProvider;
