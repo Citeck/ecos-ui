134ffa3ffdd42a78747b0dc123a9e8742e63a6dd
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-08-10 13:16:28 +0300

    ECOSUI-2503 Disable or hide Change password for Keycloak enabled installations

--- src/components/Records/actions/handler/executor/EditPasswordAction.js
+++ src/components/Records/actions/handler/executor/EditPasswordAction.js
@@ -1,28 +1,7 @@
-import { SourcesId } from '../../../../../constants';
-import WidgetService from '../../../../../services/WidgetService';
-import { UserApi } from '../../../../../api/user';
-import { getCurrentUserName } from '../../../../../helpers/util';
 import ActionsExecutor from '../ActionsExecutor';
 
 export default class EditPasswordAction extends ActionsExecutor {
-  static ACTION_ID = 'edit-password';
-
-  #userApi = new UserApi();
-
-  async execForRecord(record, action, context) {
-    return new Promise(async resolve => {
-      const modal = WidgetService.openEditorPassword();
-      const authUserData = await this.#userApi.getUserDataByRef(`${SourcesId.PERSON}@${getCurrentUserName()}`);
-      const editUserData = await this.#userApi.getUserDataByRef(record.id);
-
-      modal.open({
-        ...editUserData,
-        isAdminEditor: authUserData.isAdmin,
-        isCurrentUser: authUserData.userName === editUserData.userName,
-        isLoading: false
-      });
-    });
-  }
+  static ACTION_ID = 'change-user-password';
 
   getDefaultActionModel() {
     return {
