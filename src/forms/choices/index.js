91946042dfa8c7a1bde44fcf791099491dfe4411
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-08-10 21:10:55 +0300

    ECOSUI-2736 Bugs in crm change status form

--- src/forms/choices/index.js
+++ src/forms/choices/index.js
@@ -31,7 +31,7 @@ Choices.prototype.showDropdown = function(preventInputFocus) {
   this.dropdown.show();
 
   requestAnimationFrame(() => {
-    this.containerOuter.open(this.dropdown.distanceFromTopWindow());
+    this.recalcDropdownPosition(preventInputFocus);
 
     if (!preventInputFocus && this._canSearch) {
       this.input.focus();
@@ -41,6 +41,41 @@ Choices.prototype.showDropdown = function(preventInputFocus) {
   });
 
   return this;
+};
+
+Choices.prototype.recalcDropdownPosition = function(preventInputFocus) {
+  try {
+    const modalWrapper = this.containerInner.element.closest('.modal.show');
+    const containerSizes = this.containerInner.element.getBoundingClientRect();
+    const needToFlip = this.containerOuter.shouldFlip(containerSizes.top + this.dropdown.element.offsetHeight);
+
+    let top = containerSizes.top + containerSizes.height;
+    let left = containerSizes.left;
+
+    if (needToFlip) {
+      top = containerSizes.top - this.dropdown.element.offsetHeight - containerSizes.height + 28;
+    }
+
+    const dropdownSizes = this.dropdown.element.getBoundingClientRect();
+
+    if (modalWrapper) {
+      const modalContent = this.containerInner.element.closest('.modal-content');
+      const modalContentSizes = modalContent.getBoundingClientRect();
+
+      left -= modalContentSizes.left;
+      top = top - modalContentSizes.top;
+    }
+
+    this.dropdown.element.style.position = 'fixed';
+    this.dropdown.element.style.left = `${left}px`;
+    this.dropdown.element.style.top = `${top}px`;
+    this.dropdown.element.style.width = `${dropdownSizes.width}px`;
+    this.dropdown.element.style.minHeight = `${dropdownSizes.height}px`;
+
+    this.containerOuter.open(containerSizes.top + this.dropdown.element.offsetHeight);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export default Choices;
