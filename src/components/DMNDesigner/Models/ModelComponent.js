13a9e8b00f3f95e33f57929b49e1630c88a4efd4
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-10-04 21:49:56 +0300

    ECOSUI-2741 BPMN: lag when loading BPMN editor

---
+++ src/components/DMNDesigner/Models/ModelComponent.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Row } from 'reactstrap';
+import moment from 'moment';
+
+import ModelList from '../../designerCommon/ModelList';
+import ModelCard from '../../designerCommon/ModelCard';
+import CreateModelCard from '../../designerCommon/CreateModelCard';
+import { ViewTypes } from '../../../constants/commonDesigner';
+
+const Models = ({
+  categoryId,
+  models,
+  searchText,
+  viewType,
+  onViewLinkClick,
+  onEditLinkClick,
+  onDeleteModelClick,
+  onEditMetaClick,
+  showModelCreationForm,
+  createModelCardLabel
+}) => {
+  const ModelComponent = viewType === ViewTypes.LIST ? ModelList : ModelCard;
+
+  return (
+    <Row noGutters>
+      {models.map(model => (
+        <ModelComponent
+          canWrite={model.canWrite}
+          key={model.id}
+          viewLink={`/v2/dashboard?recordRef=${model.id}`}
+          onViewLinkClick={onViewLinkClick}
+          onEditLinkClick={e => onEditLinkClick(e, model.id)}
+          onDeleteModelClick={e => onDeleteModelClick(e, model.id)}
+          onEditMetaClick={e => onEditMetaClick(e, model.id)}
+          label={model.label}
+          author={model.creator}
+          datetime={moment(model.created).calendar()}
+          image={model.previewUrl}
+          definition={model.definition}
+        />
+      ))}
+      {viewType === ViewTypes.CARDS && !models.length && !searchText && (
+        <CreateModelCard showModelCreationForm={showModelCreationForm} label={createModelCardLabel} categoryId={categoryId} />
+      )}
+    </Row>
+  );
+};
+
+export default Models;
