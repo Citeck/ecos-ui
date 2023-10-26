be4ab23d5f9fd70d2a70c485727350787156c790
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2023-10-26 09:40:56 +0300

    ECOSENT-2793 BPMN scheme numeration

--- src/components/ModelEditor/BPMNModeler/modules/index.js
+++ src/components/ModelEditor/BPMNModeler/modules/index.js
@@ -2,22 +2,22 @@ import BpmnColorPickerModule from 'bpmn-js-color-picker';
 
 import EcosTaskPalette from './ecosTask/EcosTaskPalette';
 import EcosTaskRenderer from './ecosTask/EcosTaskRenderer';
-import EcosNumberRenderer from './ecosTask/EcosNumberRenderer';
+// import EcosNumberRenderer from './ecosTask/EcosNumberRenderer';
 import EcosTaskContextPad from './ecosTask/EcosTaskContextPad';
 import lintModule from './linter';
 
 export const onlyRenderer = {
-  __init__: ['customRenderer', 'numberRenderer'],
-  numberRenderer: ['type', EcosNumberRenderer],
+  __init__: ['customRenderer'],
+  // numberRenderer: ['type', EcosNumberRenderer],
   customRenderer: ['type', EcosTaskRenderer]
 };
 
 export default [
   {
-    __init__: ['customContextPad', 'customPalette', 'customRenderer', 'numberRenderer'],
+    __init__: ['customContextPad', 'customPalette', 'customRenderer'],
     customContextPad: ['type', EcosTaskContextPad],
     customPalette: ['type', EcosTaskPalette],
-    numberRenderer: ['type', EcosNumberRenderer],
+    // numberRenderer: ['type', EcosNumberRenderer],
     customRenderer: ['type', EcosTaskRenderer]
   },
   lintModule,
