5e4a0e59ceeec463cc6288e484ffe655cdb2e2a7
anastasia.turaeva <anastasia.turaeva@citeck.ru>
2023-11-01 16:12:39 +0300

    ECOSUI_2798 Document widget. Download all files

--- src/components/widgets/Documents/parts/Panel.js
+++ src/components/widgets/Documents/parts/Panel.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import { Labels } from '../../../../constants/documents';
+import { t } from '../../../../helpers/util';
 import { Search } from '../../../common';
 import { Dropdown } from '../../../common/form';
+import { Btn } from '../../../common/btns';
 
 export default React.memo(props => {
   const {
+    allDocuments,
+    isLoadingDownload,
+    downloadAllDocuments,
     dynamicTypes,
     statusFilter,
     selectedType,
@@ -18,6 +24,9 @@ export default React.memo(props => {
     scrollbarHeightMax,
     isMobile
   } = props;
+
+  const loading = isLoadingDownload || allDocuments === null;
+  const disabled = loading || (allDocuments && !allDocuments.length);
 
   return (
     <div ref={forwardedRef} className={classNames('ecos-docs__panel', { 'ecos-docs__panel_mobile': isMobile })}>
@@ -36,6 +45,9 @@ export default React.memo(props => {
           scrollbarHeightMax={scrollbarHeightMax}
         />
       )}
+      <Btn loading={loading} disabled={disabled} onClick={() => downloadAllDocuments(allDocuments)}>
+        {t(Labels.DOWNLOAD_ALL_DOCUMENTS)}
+      </Btn>
     </div>
   );
 });
