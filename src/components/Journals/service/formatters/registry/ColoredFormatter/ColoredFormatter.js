09fbaa9c59aa675d9f029f1073d4b0715103544f
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2023-12-12 12:10:39 +0300

    ECOSENT-2864 Add the ability to enter a condition for a color formatter

--- src/components/Journals/service/formatters/registry/ColoredFormatter/ColoredFormatter.js
+++ src/components/Journals/service/formatters/registry/ColoredFormatter/ColoredFormatter.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import isString from 'lodash/isString';
-
-import './ColoredFormatter.scss';
+import isFunction from 'lodash/isFunction';
 
 import BaseFormatter from '../../BaseFormatter';
 import CellType from '../../CellType';
+import Records from '../../../../../Records';
+
+import './ColoredFormatter.scss';
 
 export default class ColoredFormatter extends BaseFormatter {
   static TYPE = 'colored';
 
   format(props) {
-    const { cell, config = {} } = props;
+    const { cell, row, config = {}, valueIndex: index } = props;
     const color = config.color || '#000000';
+    const script = config.fn;
 
     let backgroundColor = config.defaultColor || '#FFFFFF';
     if (!isString(color)) {
@@ -24,11 +27,36 @@ export default class ColoredFormatter extends BaseFormatter {
     };
 
     let text = '';
+
     if (config.textHidden !== true) {
       text = cell.disp;
     } else {
       style['display'] = 'table';
       style['margin'] = '0 auto';
+    }
+
+    const args = { Records, cell, row, index };
+
+    /**
+     * @type {?String} HEX or RGB(A) value
+     */
+    let scriptResult;
+
+    if (isString(script)) {
+      const entries = Object.entries(args);
+      const fnArgNames = entries.map(e => e[0]);
+      const fnArgValues = entries.map(e => e[1]);
+
+      // eslint-disable-next-line
+      scriptResult = new Function(...fnArgNames, script)(...fnArgValues);
+    } else if (isFunction(script)) {
+      scriptResult = script(args);
+    } else {
+      throw new Error('Unknown fn type: ' + typeof script);
+    }
+
+    if (scriptResult) {
+      style = { ...style, color: scriptResult };
     }
 
     return (
