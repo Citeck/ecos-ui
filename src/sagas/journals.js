c69e700ebd91cd463a9b5b0f79b7435776ac4498
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2024-02-19 22:44:35 +0300

    ECOSUI-2869 Journal multiple agregation by filter

--- src/sagas/journals.js
+++ src/sagas/journals.js
@@ -558,8 +558,6 @@ export function* getGridData(api, params, stateId) {
   }
 
   for (const column of columns) {
-    const attrIndex = Object.keys(journalData.attributes).length;
-
     if (isString(column.attribute) && column.attribute.startsWith('_custom_')) {
       const predicate = ParserPredicate.replacePredicatesType(JournalsConverter.cleanUpPredicate([column.customPredicate]));
       const customData = yield call([JournalsService, JournalsService.getJournalData], journalConfig, {
@@ -568,10 +566,13 @@ export function* getGridData(api, params, stateId) {
         attributes: [column.originSchema]
       });
 
-      get(customData, 'records', []).forEach((value, key) => {
-        journalData.attributes[attrIndex] = `${column.column}?num`;
-        journalData.data[key][column.column] = value[0];
-        journalData.data[key]['rawAttributes'][`${column.column}?num`] = value[0];
+      const mappingAttribute = get(journalData, 'attributes[0]');
+      get(journalData, 'data', []).map((record, _index) => {
+        const additionalRecord = get(customData, 'records', []).find(({ rawAttributes }) =>
+          isEqual(record.rawAttributes[mappingAttribute], rawAttributes[mappingAttribute])
+        );
+        set(record, column.column, additionalRecord ? additionalRecord['0'] : '0');
+        return record;
       });
     }
   }
