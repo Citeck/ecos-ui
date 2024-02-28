837869c8bc5995831244bdb7fb2637f1bbd89ddd
maxim.dozorov <maxim.dozorov@citeck.ru>
2024-02-28 15:37:42 +0300

    ECOSUI-2859: some fixes for schema widget

--- src/pages/BpmnAdminProcessDashboard/BpmnAdminProcessDashboard.js
+++ src/pages/BpmnAdminProcessDashboard/BpmnAdminProcessDashboard.js
@@ -2,20 +2,34 @@ import React, { useContext } from 'react';
 
 import { t } from '../../helpers/util';
 import InfoPanel from './InfoPanel';
-import ProcessBpmn from './ProcessBpmn';
 import { ProcessContext } from './ProcessContext';
 import ProcessJournalWidget from './ProcessJournalWidget';
+import { selectProcessMetaInfo } from '../../selectors/processAdmin';
+import labels from "./Labels";
 
 import './style.scss';
+
+const BpmnSchema = React.lazy(() => import('../../components/BpmnSchema'));
 
 const BpmnAdminProcessDashboard = () => {
   const { activityElement, processId, processKey } = useContext(ProcessContext);
 
+  const selectMetaInfo = (store, props) => {
+    return selectProcessMetaInfo(store, {...props, processId});
+  }
+
   return (
     <div className="bpmn-admin-process-page">
       <h5 className="bpmn-admin-process-page__title">{t('bpmn-admin.process-administration', { processKey })}</h5>
+
       <InfoPanel processId={processId} />
-      <ProcessBpmn activityElement={activityElement} processId={processId} />
+
+      <BpmnSchema
+        labels={labels}
+        selectMetaInfo={selectMetaInfo}
+        activityElement={activityElement}
+      />
+
       <ProcessJournalWidget processId={processId} />
     </div>
   );
