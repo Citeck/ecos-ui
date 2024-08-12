e1909956b2881fe0367e5664b7c98dd34bcd741b
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2024-08-12 16:19:28 +0700

    ECOSUI-3066 - CLONE - Attorney. Some dependent field values are not automatically updated

--- src/forms/components/override/datetime/DateTime.js
+++ src/forms/components/override/datetime/DateTime.js
@@ -1,7 +1,10 @@
 import isEqual from 'lodash/isEqual';
+import isString from 'lodash/isString';
+import isDate from 'lodash/isDate';
 import omitBy from 'lodash/omitBy';
 import FormIODateTimeComponent from 'formiojs/components/datetime/DateTime';
 import isEmpty from 'lodash/isEmpty';
+import { parseISO, isValid } from 'date-fns';
 
 export default class DateTimeComponent extends FormIODateTimeComponent {
   build(state) {
@@ -13,6 +16,40 @@ export default class DateTimeComponent extends FormIODateTimeComponent {
     });
   }
 
+  setValue(value, flags) {
+    if (isString(value)) {
+      const parsedDate = parseISO(value);
+
+      if (isValid(parsedDate)) {
+        value = parsedDate.toISOString().replace('.000Z', 'Z');
+      } else {
+        return;
+      }
+    } else if (isDate(value)) {
+      value = value.toISOString().replace('.000Z', 'Z');
+    }
+
+    super.setValue(value, flags);
+  }
+
+  getValue() {
+    const value = super.getValue();
+
+    if (isString(value)) {
+      const parsedDate = parseISO(value);
+
+      if (isValid(parsedDate)) {
+        return parsedDate.toISOString().replace('.000Z', 'Z');
+      } else {
+        return '';
+      }
+    } else if (isDate(value)) {
+      return value.toISOString().replace('.000Z', 'Z');
+    }
+
+    return value;
+  }
+
   static optimizeSchema(comp) {
     const defaultSchema = DateTimeComponent.schema();
 
@@ -20,7 +57,6 @@ export default class DateTimeComponent extends FormIODateTimeComponent {
     if (comp.timePicker) {
       comp.timePicker = omitBy(comp.timePicker, (value, key) => isEqual(defaultSchema.timePicker[key], value));
     }
-
     return omitBy(comp, (value, key) => {
       if (['datePicker', 'timePicker'].includes(key) && isEmpty(value)) {
         return true;
