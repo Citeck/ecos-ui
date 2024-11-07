8d0aad1de45cc8465e444c4cda4f9a68d5eb87bb
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2024-11-07 13:04:55 +0300

    ECOSUI-3133 Orstructure. double scrolls

--- src/pages/Orgstructure/components/Body/Body.js
+++ src/pages/Orgstructure/components/Body/Body.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 import { t } from '../../../../helpers/util';
@@ -10,23 +10,18 @@ import List from './List';
 import './Body.scss';
 
 const Body = ({ reloadList, tabId, toggleToFirstTab }) => {
-  const [isSafari, setSafari] = useState(false);
   const context = useContext(OrgstructContext);
   const { currentTab, tabItems, isSearching } = context;
 
   const children = tabItems[currentTab].filter(i => !i.parentId);
 
   const renderView = props => {
-    return <div {...props} style={{ ...props.style, marginBottom: '-13px' }} />;
+    return <div {...props} style={{ ...props.style, marginBottom: '140px' }} />;
   };
-
-  if (/Safari/.test(navigator.userAgent) && !/Chrome|CriOS|YaBrowser|OPR|Edg/.test(navigator.userAgent) && !isSafari) {
-    setSafari(true);
-  }
 
   return (
     <div className="select-orgstruct__body">
-      <Scrollbars className="slide-menu-list" renderView={renderView} style={{ ...(isSafari && { position: 'initial' }) }}>
+      <Scrollbars className="slide-menu-list" renderView={renderView} style={{ position: 'initial' }}>
         <div className="select-orgstruct__list-wrapper">
           {isSearching && <Loader blur />}
           {!children.length && !isSearching && <InfoText text={t('select-orgstruct.empty-list')} />}
