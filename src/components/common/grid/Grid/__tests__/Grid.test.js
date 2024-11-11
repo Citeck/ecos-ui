560ab44e9dc152561827daebf033c202bc387cf1
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2024-11-11 17:05:01 +0700

    ECOSUI-3117 - Full height view
    Fix broken Grid tests

--- src/components/common/grid/Grid/__tests__/Grid.test.js
+++ src/components/common/grid/Grid/__tests__/Grid.test.js
@@ -1,9 +1,3 @@
-global.ResizeObserver = class {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-};
-
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
