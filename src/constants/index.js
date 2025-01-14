9c35ee4e7b55e0c4d62d6dddab276bb406fbfbb1
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2025-01-14 13:15:23 +0700

    ECOSUI-3143 - [UI] Add workspace support for dashboards

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,7 +39,7 @@ export const URL = {
 };
 
 export const RELOCATED_URL = {
-  [URL.BPMN_DESIGNER]: URL.ADMIN_PAGE
+  [URL.BPMN_DESIGNER]: `${URL.ADMIN_PAGE}?type=${SectionTypes.BPM}`
 };
 
 export const URL_MATCHING = {
