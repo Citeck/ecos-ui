12abc04e6c2a369f1e2aa775144ef4f52faa3abb
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2025-01-22 12:05:27 +0300

    ECOSUI-3203 Publication page. Custom widget

--- src/components/widgets/Components.js
+++ src/components/widgets/Components.js
@@ -36,6 +36,7 @@ export const ComponentKeys = {
   PROCESS_STATISTICS: 'process-statistics',
   STAGES: 'stages',
   CHARTS: 'charts',
+  PUBLICATION: 'publication',
   KANBAN_BOARD: 'kanban-board'
 };
 
@@ -248,6 +249,22 @@ export default class Components {
       label: 'dashboard-settings.widget.charts',
       supportedDashboardTypes: [DashboardTypes.CASE_DETAILS, DashboardTypes.USER, DashboardTypes.CUSTOM]
     },
+    [ComponentKeys.PUBLICATION]: {
+      load: () =>
+        lazy(() =>
+          import('../../plugins').then(plugins => ({
+            default: get(plugins, 'default.PublicationWidget', () => null)
+          }))
+        ),
+      additionalProps: {
+        isDragDisabledByLayout: layout =>
+          layout &&
+          (!layout.columns || (layout.columns.length !== 1 && !layout.columns.find(column => Array.isArray(column) && column.length === 1)))
+      },
+      checkIsAvailable: () => Boolean(get(window, 'Citeck.Plugins.PublicationWidget')),
+      label: 'dashboard-settings.widget.publication',
+      supportedDashboardTypes: [DashboardTypes.CASE_DETAILS]
+    },
     [ComponentKeys.STAGES]: {
       load: () =>
         lazy(() =>
@@ -274,7 +291,7 @@ export default class Components {
     }
   });
 
-  static allDashboardsComponents = [ComponentKeys.JOURNAL, ComponentKeys.WEB_PAGE];
+  static allDashboardsComponents = [ComponentKeys.JOURNAL, ComponentKeys.WEB_PAGE, ComponentKeys.PUBLICATION];
 
   static get allDashboardTypes() {
     return Object.values(DashboardTypes);
---
+++ src/plugins/ecos-ui-publication-widget-plugin/Widget/constants.js
@@ -0,0 +1,18 @@
+export const Labels = {
+  Settings: {
+    SETTINGS_TITLE: 'publication-widget.settings.title',
+    TITLE_FIELD: 'publication-widget.settings.field.title',
+    JOURNAL_FIELD: 'publication-widget.settings.field.journal',
+    BOARD_FIELD: 'publication-widget.settings.field.board',
+    BOARD_LIST: 'publication-widget.settings.field.board-list',
+    TEMPLATE_FIELD: 'publication-widget.settings.field.template',
+    TEMPLATE_LIST: 'publication-widget.settings.field.template-list',
+    INVALID_JOURNAL_ID: 'publication-widget.settings.invalid-journal-id',
+    SETTINGS_BNT_CANCEL: 'publication-widget.settings.bnt.cancel',
+    SETTINGS_BNT_SAVE: 'publication-widget.settings.bnt.save'
+  },
+  Widget: {
+    WIDGET_TITLE: 'publication-widget.title',
+    WIDGET_NO_JOURNAL: 'publication-widget.no-journal'
+  }
+};
