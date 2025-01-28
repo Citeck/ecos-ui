0aa30b1f37be9d223f0259c53ca4e72231dcc18f
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2025-01-28 13:10:16 +0700

    ECOSUI-3221 - Kanban. Date Grid

--- src/services/pageTabs/PageTabList.js
+++ src/services/pageTabs/PageTabList.js
@@ -394,7 +394,6 @@ class PageTabList {
     }
 
     const wsTabs = this.tabs.filter(tab => tab.workspace === wsId);
-    console.log('wsTabs:', wsTabs);
     return wsTabs.find(tab => get(tab, 'isLastActive') === true);
   };
 
