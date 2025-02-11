c1f52edb3bf342a66a3c7d6ac6b6fb69001645bb
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2025-02-11 11:02:47 +0300

    ECOSUI-3204 - Widget "Publications"

--- src/components/common/icons/PreviewList.jsx
+++ src/components/common/icons/PreviewList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default ({ width = 20, height = 22, viewBox = '0 0 20 22', fill = 'none' }) => (
+export default ({ width = 22, height = 24, viewBox = '0 0 20 22', fill = 'none' }) => (
   <svg width={width} height={height} viewBox={viewBox} fill={fill} xmlns="http://www.w3.org/2000/svg">
     <rect x="1" y="2" width="6" height="11" rx="2" fill="#b7b7b7" />
     <rect x="10" y="6" width="4" height="9" rx="2" transform="rotate(-90 10 6)" fill="#b7b7b7" />
