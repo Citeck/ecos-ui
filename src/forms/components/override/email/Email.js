b9176e90ef91a5e8f48664b90d1f40dc736a1693
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2025-02-14 16:53:15 +0700

    ECOSUI-3241 - Form. Date/Time+email. Incorrect display component

--- src/forms/components/override/email/Email.js
+++ src/forms/components/override/email/Email.js
@@ -1,3 +1,5 @@
+import get from 'lodash/get';
+import isEqual from 'lodash/isEqual';
 import FormIOEmailComponent from 'formiojs/components/email/Email';
 import { overrideTriggerChange } from '../misc';
 
@@ -6,5 +8,38 @@ export default class EmailComponent extends FormIOEmailComponent {
     super(...args);
 
     overrideTriggerChange.call(this);
+  }
+
+  isElementOrParentsHidden() {
+    let current = this;
+
+    while (!!current) {
+      if (get(current, 'element.hidden') === true) {
+        return true;
+      }
+      current = get(current, 'parent', null);
+    }
+
+    return false;
+  }
+
+  refresh(value, refreshOnKey) {
+    if (this.hasOwnProperty('refreshOnValue')) {
+      this.refreshOnChanged = !isEqual(value, this.refreshOnValue[refreshOnKey]);
+      this.refreshOnValue[refreshOnKey] = value;
+    } else {
+      this.refreshOnChanged = true;
+      this.refreshOnValue = {
+        [refreshOnKey]: value
+      };
+    }
+
+    if (this.refreshOnChanged && !this.isElementOrParentsHidden()) {
+      if (this.component.clearOnRefresh) {
+        this.setValue(null);
+      }
+
+      this.triggerRedraw();
+    }
   }
 }
