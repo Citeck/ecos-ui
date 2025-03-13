d15f4b7b97db18e0aa8a67418c5f5eceb6e7c09b
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2025-03-13 16:30:44 +0700

    ECOSUI-3285 - Bug list. New frontend architecture
    Fix d&d

--- src/components/Drag-n-Drop/SortableContainer.js
+++ src/components/Drag-n-Drop/SortableContainer.js
@@ -1,4 +1,4 @@
-import { SortableContainer as sortableContainer } from 'react-sortable-hoc';
+import { SortableContainer as sortableContainer } from '@/services/sortableHoc';
 
 export const SortableContainer = sortableContainer(({ children }) => {
   return children;
