8689ad6e7fe60e1c0e7dba4e9185e713879a6798
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2025-04-17 15:35:54 +0300

    ECOSUI-3317 Group action. The selection of records is not reset after performing a group action

--- src/components/Journals/GroupActions/GroupActions.js
+++ src/components/Journals/GroupActions/GroupActions.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState, useRef, useMemo } from 'react';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
 import get from 'lodash/get';
@@ -21,22 +21,23 @@ const Labels = {
   SELECTED: 'journal.group-actions.selected-records.label',
   SELECTED_SHORT: 'journal.group-actions.selected-records.label_short',
   SELECTED_COUNT: 'journal.group-actions.selected-records.label-count',
-  RESULT: 'journal.group-actions.filter-result',
+  RESULT: 'journal.group-actions.filter-result'
 };
 
 const TYPE_ACT = {
   QUERY: 'forQuery',
-  RECORDS: 'forRecords',
+  RECORDS: 'forRecords'
 };
 
 const GroupActions = React.memo(
-  (props) => {
+  props => {
     const [isOpenRecActions, setOpenRecActions] = useState(false);
     const [isOpenQueryActions, setOpenQueryActions] = useState(false);
     const [targetPrefix] = useState(uniqueId('group-actions-'));
     const [recordsActions, setRecordsActions] = useState([]);
     const [queryActions, setQueryActions] = useState([]);
     const [label, setLabel] = useState('');
+    const recordsLength = useRef();
 
     const {
       isMobile,
@@ -47,7 +48,7 @@ const GroupActions = React.memo(
       execRecordsAction,
       isFilterOn,
       isSeparateActionListForQuery,
-      excludedRecords,
+      excludedRecords
     } = props;
 
     const total = get(grid, 'total', 0);
@@ -55,30 +56,39 @@ const GroupActions = React.memo(
     const selected = selectAllRecordsVisible ? total - excludedRecords.length : selectedLen;
     const labelRecActionsCount = t(Labels.SELECTED_COUNT, { selected, total });
 
-    const labelRecActions = useCallback(
-      () => t(Labels.SELECTED_SHORT, { data: selectedLen === 0 ? grid.total : labelRecActionsCount }),
-      [total, selected, selectedLen, grid.total],
-    );
+    const labelRecActions = useMemo(() => t(Labels.SELECTED_SHORT, { data: selectedLen === 0 ? grid.total : labelRecActionsCount }), [
+      total,
+      selected,
+      selectedLen,
+      grid.total
+    ]);
 
     if (total < recordsLength.current && selectedRecords.length) {
       deselectAllRecords();
       selectedRecords.length = 0;
     }
 
-    useEffect(() => {
-      setLabel(labelRecActions);
-    }, [labelRecActions]);
+    useEffect(
+      () => {
+        recordsLength.current = total;
+        setLabel(labelRecActions);
+      },
+      [labelRecActions]
+    );
 
-    useEffect(() => {
-      const recordsActions = get(grid, 'actions.forRecords.actions', []).map((item) => ({ ...item, _typeAct: TYPE_ACT.RECORDS }));
-      const queryActions = get(grid, 'actions.forQuery.actions', []).map((item) => ({ ...item, _typeAct: TYPE_ACT.QUERY }));
+    useEffect(
+      () => {
+        const recordsActions = get(grid, 'actions.forRecords.actions', []).map(item => ({ ...item, _typeAct: TYPE_ACT.RECORDS }));
+        const queryActions = get(grid, 'actions.forQuery.actions', []).map(item => ({ ...item, _typeAct: TYPE_ACT.QUERY }));
 
-      setRecordsActions(recordsActions);
-      setQueryActions(queryActions);
-    }, [grid, isSeparateActionListForQuery]);
+        setRecordsActions(recordsActions);
+        setQueryActions(queryActions);
+      },
+      [grid, isSeparateActionListForQuery]
+    );
 
     const handleExecuteAction = useCallback(
-      (action) => {
+      action => {
         const context = { excludedRecords };
 
         if (selectAllRecordsVisible || selectedLen === 0) {
@@ -90,23 +100,23 @@ const GroupActions = React.memo(
           execRecordsAction(selectedRecords, action);
         }
       },
-      [grid, isFilterOn, selectedRecords, selectedLen, excludedRecords],
+      [grid, isFilterOn, selectedRecords, selectedLen, excludedRecords]
     );
 
     const getItemClassName = useCallback(
-      (action) => {
+      action => {
         const disabled = !isFilterOn && selectAllRecordsVisible;
         const isForQuery = action._typeAct === TYPE_ACT.QUERY;
 
         return classNames('ecos-group-actions__dropdown-item', {
           'ecos-group-actions__dropdown-item_disabled': (selectedLen === 0 && !action.features.execForQuery) || disabled,
-          'ecos-group-actions__dropdown-item_query': isForQuery,
+          'ecos-group-actions__dropdown-item_query': isForQuery
         });
       },
-      [selectAllRecordsVisible, isFilterOn, selectedLen],
+      [selectAllRecordsVisible, isFilterOn, selectedLen]
     );
 
-    const iconOpener = useCallback((flag) => classNames('ecos-btn__i_right', { 'icon-small-up': flag, 'icon-small-down': !flag }), []);
+    const iconOpener = useCallback(flag => classNames('ecos-btn__i_right', { 'icon-small-up': flag, 'icon-small-down': !flag }), []);
 
     return (
       <>
@@ -151,7 +161,7 @@ const GroupActions = React.memo(
           >
             <TwoIcoBtn
               className={classNames('ecos-btn_hover_blue2 ecos-btn_grey3 ecos-group-actions__control', {
-                'ecos-group-actions__control_mobile': isMobile,
+                'ecos-group-actions__control_mobile': isMobile
               })}
               icons={[classNames({ 'icon-filter icon_semantic': isMobile }), iconOpener(isOpenQueryActions)]}
               id={targetPrefix}
@@ -167,7 +177,7 @@ const GroupActions = React.memo(
     isEqual(prevProps.stateId, nextProps.stateId) &&
     isEqual(prevProps.grid, nextProps.grid) &&
     isEqual(prevProps.selectAllRecordsVisible, nextProps.selectAllRecordsVisible) &&
-    isEqual(prevProps.selectedRecords, nextProps.selectedRecords),
+    isEqual(prevProps.selectedRecords, nextProps.selectedRecords)
 );
 
 const mapStateToProps = (state, props) => {
@@ -176,7 +186,7 @@ const mapStateToProps = (state, props) => {
   return {
     isMobile: isNil(props.isMobile) ? get(state, 'view.isMobile') : props.isMobile,
     isSeparateActionListForQuery: get(state, 'app.isSeparateActionListForQuery', false),
-    ...ownState,
+    ...ownState
   };
 };
 
@@ -185,10 +195,13 @@ const mapDispatchToProps = (dispatch, props) => {
 
   return {
     execRecordsAction: (records, action, context) => dispatch(execRecordsAction(w({ records, action, context }))),
-    deselectAllRecords: () => dispatch(deselectAllRecords(w())),
+    deselectAllRecords: () => dispatch(deselectAllRecords(w()))
   };
 };
 
 export { GroupActions };
 
-export default connect(mapStateToProps, mapDispatchToProps)(GroupActions);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GroupActions);
