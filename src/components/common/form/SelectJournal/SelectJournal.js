9ac8db698579f0e4901faa9c941906f709841a45
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2025-06-24 16:21:33 +0700

    ECOSUI-3358 - Select journal. Journal is not updating

--- src/components/common/form/SelectJournal/SelectJournal.js
+++ src/components/common/form/SelectJournal/SelectJournal.js
@@ -653,11 +653,13 @@ export default class SelectJournal extends Component {
     this.setState({ isSelectModalOpen: true });
 
     if (!isJournalConfigFetched) {
-      this.getJournalConfig().then(this.refreshGridData);
+      this.fetchJournalData();
     } else if (!isGridDataReady) {
       this.refreshGridData();
     }
   };
+
+  fetchJournalData = () => this.getJournalConfig().then(this.refreshGridData);
 
   onCreateFormSubmit = (record, form, alias) => {
     const { multiple } = this.props;
@@ -670,6 +672,11 @@ export default class SelectJournal extends Component {
       ? Math.floor(gridData.total / pagination.maxItems) * pagination.maxItems
       : paginationInitState.skipCount;
     const newPageNum = isAscending ? Math.ceil((gridData.total + 1) / pagination.maxItems) : paginationInitState.page;
+
+    if (!alias) {
+      this.fetchJournalData();
+      return;
+    }
 
     alias.toJsonAsync(true).then(res => {
       const newData = cloneDeep(this.state);
@@ -684,7 +691,7 @@ export default class SelectJournal extends Component {
         pagination: { skipCount: newSkipCount, page: newPageNum }
       });
 
-      this.setState(newData, this.refreshGridData);
+      this.setState(newData, this.fetchJournalData);
     });
   };
 
