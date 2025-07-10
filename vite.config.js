b312fe6862aaef8708205b292a608e62383a268a
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2025-07-10 12:39:13 +0700

    ECOSUI-3470 - New graphic type

--- vite.config.js
+++ vite.config.js
@@ -143,7 +143,8 @@ export default defineConfig(({ mode }) => {
           find: '@',
           replacement: path.resolve(__dirname, 'src')
         }
-      ]
+      ],
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
     },
     plugins: [
       nodePolyfills({
