a4de8940e29a53c0212dfed0b329f61bb5dded14
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2025-10-01 19:52:47 +0300

    ECOSUI-3503 Gant chart creation

--- Widget/Widget.svelte
+++ Widget/Widget.svelte
@@ -62,9 +62,9 @@
 {#if $ganttStore.isLoading}
   <div
     class="gantt-loader"
-    style="display: flex; justify-content: center; align-items: center; height: 100%; font-size: 16px;"
+    style="display: flex; justify-content: center; align-items: center; height: 100%; padding: 200px;"
   >
-    Loading Gantt chart...
+    <svg width="80" height="80" viewBox="-20 -20 42 42" xmlns="http://www.w3.org/2000/svg" stroke="var(--primary)"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".5" cx="0" cy="0" r="20" stroke="var(--primary)" stroke-width="2"></circle><path d="M20 0c0-9.94-8.06-20-20-20"><animateTransform attributeName="transform" type="rotate" from="0 0 0" to="360 0 0" dur="1s" repeatCount="indefinite"></animateTransform></path></g></g></svg>
   </div>
 {:else if !$ganttStore.tasks && !ganttSettingsRef}
   <div>No Gantt settings reference provided</div>
