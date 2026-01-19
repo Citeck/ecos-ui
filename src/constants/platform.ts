97a6a9594b8df29d921a98e38d3851adf47f525f
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2026-01-19 12:14:59 +0300

    ECOSUI-3178 About window

--- src/constants/platform.ts
+++ src/constants/platform.ts
@@ -1,4 +1,4 @@
 export const PLATFORM_NAME = 'Citeck';
 export const PLATFORM_LINK_COMMUNITY = 'https://t.me/citeck';
 
-export const PLATFORM_COPYRIGHT = `Copyright 2012 - ${new Date().getFullYear()} ${PLATFORM_NAME}`;
+export const PLATFORM_COPYRIGHT = `© 2012 - ${new Date().getFullYear()} ${PLATFORM_NAME}`;
