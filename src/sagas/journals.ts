5c06dad9378f547fbf244b707d01fb72c653da51
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2026-04-30 09:31:24 +0300

    COREDEV-3719 - Journal: sometimes entered value is lost

--- src/sagas/journals.ts
+++ src/sagas/journals.ts
@@ -86,7 +86,7 @@ import { ParserPredicate } from '@/components/Filters/predicates';
 import { WidgetsConfigType } from '@/components/Journals/JournalsPreviewWidgets/JournalsPreviewWidgets';
 import { DEFAULT_PAGINATION, isKanban, JOURNAL_DASHLET_CONFIG_VERSION } from '@/components/Journals/constants';
 import JournalsService, { EditorService, PresetsServiceApi } from '@/components/Journals/service';
-import { isSavedAttValueEqual } from '@/components/Journals/service/editors/editorUtils';
+import { isSavedAttValueEqual, isValidAttValueForType } from '@/components/Journals/service/editors/editorUtils';
 import { buildSaveAttKey } from '@/components/Journals/service/journalColumnsResolver';
 import Records from '@/components/Records';
 import RecordImpl from '@/components/Records/Record';
@@ -1420,6 +1420,13 @@ function* sagaSaveRecords({ api, stateId, w }: IJournalsExtraArgumentsStore, act
 
     const currentColumn = grid.columns.find(item => item.attribute === attribute);
     const valueToSave = EditorService.getValueToSave(value, currentColumn?.multiple);
+
+    if (!isValidAttValueForType(valueToSave, currentColumn?.type)) {
+      const dataWithError = grid.data.map(record => (record.id === id ? { ...record, error: attribute } : record));
+      yield put(setGrid(w({ ...grid, data: dataWithError, editingRules })));
+      return;
+    }
+
     const optimisticData = grid.data.map(record => {
       if (record.id === id) {
         return { ...record, [attribute]: value };
