f9eda961ecf1359774e60d99dadc4315cd0b4f03
pavel.marchuk <pavel.marchuk@citeck.ru>
2026-09-03 10:26:10 +0700

    EDMS-8899/COREDEV-224/ECOSUI-2220: Send parent form definitions

--- src/forms/Formio.js
+++ src/forms/Formio.js
@@ -33,4 +33,8 @@ Formio.requireLibrary = (name, property, src, polling) => {
   return originalRequireLibrary(name, property, src, polling);
 };
 
+Formio.forms = {};
+
+window.Formio = Formio;
+
 export default Formio;
--- src/forms/Webform.js
+++ src/forms/Webform.js
@@ -1,11 +1,39 @@
 import Webform from 'formiojs/Webform';
 import cloneDeep from 'lodash/cloneDeep';
+import get from 'lodash/get';
 import merge from 'lodash/merge';
 import { OUTCOME_BUTTONS_PREFIX } from '../constants/forms';
+import Formio from './Formio';
 
 const originalSetElement = Webform.prototype.setElement;
 const originalSubmit = Webform.prototype.submit;
+const originalBuild = Webform.prototype.build;
 const originalPropertyLoading = Object.getOwnPropertyDescriptor(Webform.prototype, 'loading');
+
+Webform.prototype.build = function(state) {
+  Formio.forms[this.id] = this;
+
+  return originalBuild.call(this, state);
+};
+
+Object.defineProperty(Webform.prototype, 'parentForm', {
+  get: function() {
+    const parentId = get(this, 'options.parentId');
+    const keys = Object.keys(Formio.forms);
+    const prevFormKey = keys.findIndex(i => i === this.id);
+    let penultimateForm = null;
+
+    if (prevFormKey !== -1) {
+      penultimateForm = get(Formio, ['forms', keys.slice(prevFormKey - 1, prevFormKey)[0]]) || null;
+    }
+
+    if (penultimateForm && penultimateForm.id === this.id) {
+      penultimateForm = null;
+    }
+
+    return parentId ? get(Formio, ['forms', parentId]) : penultimateForm;
+  }
+});
 
 Webform.prototype.setElement = function(element) {
   originalSetElement.call(this, element);
